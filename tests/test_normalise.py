5eb33237e1067d07cef4bd6157be17fc4b4231ef
Paul Downey <psd@whatfettle.com>
2020-03-10 08:06:25 +0000

    init test

---
+++ tests/test_normalise.py
@@ -0,0 +1,11 @@
+from digital_land.pipeline.normalise import Normaliser
+
+
+def test_init():
+    n = Normaliser()
+    assert n.null_path.endswith("patch/null.csv")
+    assert n.skip_path.endswith("patch/skip.csv")
+
+    n = Normaliser(skip_path="tests/data/skip.csv", null_path="tests/data/null.csv")
+    assert n.null_path == "tests/data/null.csv"
+    assert n.skip_path == "tests/data/skip.csv"
