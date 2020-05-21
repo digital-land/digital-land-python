cf069edfecd7098929884e696ce4fa66de93eaf6
Jon Bramley <jon.bramley@sainsburys.co.uk>
2020-05-21 17:03:19 +0100

    More unit tests for collect - move away from Spy

--- tests/unit/test_collect.py
+++ tests/unit/test_collect.py
@@ -1,38 +1,73 @@
-import responses
 import hashlib
-from datetime import datetime
+import os
+from datetime import datetime, timedelta
+
+import pytest
+import responses
+
 from digital_land.collect import Collector, FetchStatus
 
 
-class SpySaver:
-    def __init__(self):
-        self.content = ""
-        self.log = ""
-        self.log_path = ""
+@pytest.fixture
+def collector(tmp_path):
+    collector = Collector()
+    collector.resource_dir = str(tmp_path / "resource")
+    collector.log_dir = str(tmp_path / "log")
+    return collector
 
-    def save_content(self, content):
-        self.content = content.decode("utf-8")
-        return
 
-    def save_log(self, path, log):
-        self.log = log
-        self.log_path = path
+@pytest.fixture
+def prepared_response():
+    responses.add(responses.GET, "http://some.url", body="some data")
+
+
+def sha_digest(string):
+    return hashlib.sha256(string.encode("utf-8")).hexdigest()
 
 
 @responses.activate
-def test_fetch():
-    collector = Collector()
-    saver = SpySaver()
-    collector.save_content = saver.save_content
-    collector.save_log = saver.save_log
-
-    responses.add(responses.GET, "http://some.url", body="some data")
-
-    status = collector.fetch("http://some.url")
+def test_fetch(collector, prepared_response, tmp_path):
+    url = "http://some.url"
+    status = collector.fetch(url)
 
     assert status == FetchStatus.OK
-    assert saver.content == "some data"
-    assert (
-        saver.log_path
-        == f"collection/log/{datetime.now().strftime('%Y-%m-%d')}/{hashlib.sha256('http://some.url'.encode('utf-8')).hexdigest()}.json"
+    output_path = tmp_path / f"resource/{sha_digest('some data')}"
+    assert os.path.isfile(output_path)
+    assert open(output_path).read() == "some data"
+
+    assert os.path.isfile(
+        tmp_path / f"log/{datetime.now().strftime('%Y-%m-%d')}/{sha_digest(url)}.json"
     )
+
+
+@responses.activate
+def test_already_fetched(collector, prepared_response):
+    status = collector.fetch("http://some.url")
+    assert status == FetchStatus.OK
+
+    new_status = collector.fetch("http://some.url")
+    assert new_status == FetchStatus.ALREADY_FETCHED
+
+
+@responses.activate
+def test_expired(collector):
+    yesterday = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
+
+    status = collector.fetch("http://some.url", end_date=yesterday)
+
+    assert status == FetchStatus.EXPIRED
+
+
+@responses.activate
+def test_hash_check(collector, prepared_response):
+    url = "http://some.url"
+    status = collector.fetch(url, endpoint=sha_digest(url))
+
+    assert status == FetchStatus.OK
+
+
+@responses.activate
+def test_hash_failure(collector, prepared_response):
+    status = collector.fetch("http://some.url", endpoint="http://other.url")
+
+    assert status == FetchStatus.HASH_FAILURE
