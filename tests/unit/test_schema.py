b0c5ff1dbc7de0eae091ec8619e2c692819d4338
Jon Bramley <jon.bramley@sainsburys.co.uk>
2020-05-29 11:08:18 +0100

    Fixed some bugs in Schema and added more tests

--- tests/unit/test_schema.py
+++ tests/unit/test_schema.py
@@ -1,4 +1,5 @@
 from digital_land.schema import Schema
+from digital_land.datatype.enum import EnumDataType
 
 
 def test_schema_init():
@@ -32,3 +33,39 @@ def test_schema_typos():
         "una": "one",
         "uno": "one",
     }
+
+
+def test_current_fieldnames():
+    s = Schema("tests/data/schema-deprecated.json")
+    assert s.current_fieldnames() == [
+        "one",
+        "two",
+    ]
+
+
+def test_required_fieldnames():
+    s = Schema("tests/data/schema-required.json")
+    assert s.required_fieldnames() == [
+        "one",
+    ]
+
+
+def test_default_fieldnames():
+    s = Schema("tests/data/schema-default.json")
+    assert s.default_fieldnames() == {"two": ["one"]}
+
+
+def test_strip():
+    s = Schema("tests/data/schema-strip.json")
+
+    assert s.strip("one", "no-op") == "no-op"
+    assert s.strip("one", "data test-suffix") == "data"
+    assert s.strip("two", "data test-suffix") == "data test-suffix"
+    assert s.strip("one", "data test-suffix middle") == "data test-suffix middle"
+
+
+def test_field_type():
+    s = Schema("tests/data/schema-field-types.json")
+
+    assert type(s.field_type("one")) == EnumDataType
+    # TODO more field types
