9b9de6d0f88750a5b9c4241ed131322e750d902a
ksp37 <16578929+ksp37@users.noreply.github.com>
2020-11-03 17:32:40 +0000

    Implement command to add sources and endpoints (#27)

--- digital_land/update.py
+++ digital_land/update.py
@@ -1,10 +1,18 @@
 from datetime import datetime, date
-from digital_land.collection import LogRegister, EndpointRegister
+from digital_land.register import Item
+from digital_land.collection import LogRegister, EndpointRegister, SourceRegister
+from pathlib import Path
+import hashlib
+
+
+def get_source_endpoint_fieldnames():
+    return set(EndpointRegister.fieldnames + SourceRegister.fieldnames)
 
 
 def get_failing_endpoints_from_registers(
-    log_path, endpoints_path, first_date, last_date=date.today()
+    log_path, endpoints_dir, first_date, last_date=date.today()
 ):
+    endpoints_path = Path(endpoints_dir)
     log_register = LogRegister()
     endpoint_register = EndpointRegister()
     log_register.load_collection(log_path)
@@ -66,33 +74,158 @@ def has_collected_resource(log_item):
     return True, failure_reason
 
 
+def add_new_source_endpoint(resource_entry, source_path, endpoint_path):
+    source_path = Path(source_path)
+    endpoint_path = Path(endpoint_path)
+    endpoint_register = EndpointRegister()
+    source_register = SourceRegister()
+    endpoint_register.load(endpoint_path)
+    source_register.load(source_path)
+
+    resource_entry["endpoint"] = hashlib.sha256(
+        resource_entry["endpoint-url"].encode("utf-8")
+    ).hexdigest()
+    add_new_endpoint(resource_entry, endpoint_register)
+    add_new_source(resource_entry, source_register)
+
+    source_register.save(source_path)
+    endpoint_register.save(endpoint_path)
+
+
+def add_new_endpoint(resource_entry, endpoint_register):
+    if "endpoint" not in resource_entry or "organisation" not in resource_entry:
+        raise ValueError("Missing required fields 'endpoint' or 'organisation'")
+    endpoint_entries = endpoint_register.entries
+    if resource_entry["endpoint"] in endpoint_register.record:
+        existing_idx = endpoint_register.record[resource_entry["endpoint"]][0]
+        if endpoint_entries[existing_idx].item["end-date"]:
+            # Entry already exists with an end-date
+            print(
+                "WARNING: endpoint end-date {} found for URL {}".format(
+                    endpoint_entries[existing_idx].item["end-date"],
+                    resource_entry["endpoint-url"],
+                )
+            )
+        else:
+            # No op if active entry already exists
+            print(
+                "Active endpoint already exists for URL {}".format(
+                    resource_entry["endpoint-url"]
+                )
+            )
+            return
+
+    endpoint_entry = {
+        "endpoint": resource_entry["endpoint"],
+        "endpoint-url": resource_entry["endpoint-url"],
+        "entry-date": date.today().strftime("%Y-%m-%d"),
+        "end-date": datetime.strptime(resource_entry["end-date"], "%Y-%m-%d").date()
+        if "end-date" in resource_entry
+        else "",
+    }
+
+    # If empty start-date set by user then no date should be set. Otherwise set user-specified date or use current date
+    if "start-date" in resource_entry:
+        if resource_entry["start-date"]:
+            endpoint_entry["start-date"] = datetime.strptime(
+                resource_entry["start-date"], "%Y-%m-%d"
+            ).date()
+    else:
+        endpoint_entry["start-date"] = date.today().strftime("%Y-%m-%d")
+
+    endpoint_item = Item(endpoint_entry)
+    endpoint_register.add(endpoint_item)
+
+
+def add_new_source(resource_entry, source_register):
+    if "endpoint" not in resource_entry or "organisation" not in resource_entry:
+        raise ValueError("Missing required fields 'endpoint' or 'organisation'")
+    source_entries = source_register.entries
+    if resource_entry["endpoint"] in source_register.record:
+        for idx in source_register.record[resource_entry["endpoint"]]:
+            if (
+                resource_entry["organisation"]
+                == source_entries[idx].item["organisation"]
+            ):
+                if source_entries[idx].item["end-date"]:
+                    # Entry already exists with an end-date
+                    print(
+                        "WARNING: source end-date {} found for URL {}".format(
+                            source_entries[idx].item["end-date"],
+                            resource_entry["endpoint-url"],
+                        )
+                    )
+                    break
+                else:
+                    # No op if active entry already exists
+                    print(
+                        "Active source entry already exists for organisation {} and URL {}".format(
+                            resource_entry["organisation"],
+                            resource_entry["endpoint-url"],
+                        )
+                    )
+                    return
+
+    source_entry = {
+        "collection": resource_entry["collection"]
+        if "collection" in resource_entry
+        else resource_entry["pipeline"],
+        "pipeline": resource_entry["pipeline"],
+        "organisation": resource_entry["organisation"],
+        "endpoint": resource_entry["endpoint"],
+        "documentation-url": resource_entry["documentation-url"],
+        "licence": resource_entry["licence"],
+        "attribution": resource_entry["attribution"],
+        "entry-date": date.today().strftime("%Y-%m-%d"),
+        "end-date": datetime.strptime(resource_entry["end-date"], "%Y-%m-%d").date()
+        if "end-date" in resource_entry
+        else "",
+    }
+
+    # If empty start-date set by user then no date should be set. Otherwise set user-specified date or use current date
+    if "start-date" in resource_entry:
+        source_entry["start-date"] = (
+            datetime.strptime(resource_entry["start-date"], "%Y-%m-%d").date()
+            if resource_entry["start-date"]
+            else ""
+        )
+    else:
+        source_entry["start-date"] = date.today().strftime("%Y-%m-%d")
+
+    source_item = Item(source_entry)
+    source_register.add(source_item)
+
+
 def get_entries_between_keys(start_key, end_key, length, register_lookup):
     if end_key < start_key:
-        return -1, -1
+        return None, None
 
     lo = 0
     hi = length
+    return bisect_left(start_key, lo, hi, register_lookup), bisect_right(
+        end_key, lo, hi, register_lookup
+    )
 
-    # Binary search adapted from bisect module. See bisect_left and bisect_right.
-    while lo < hi:
-        mid = (lo + hi) // 2
-        key = register_lookup(mid)
-        if key < start_key:
-            lo = mid + 1
+
+def bisect_left(key, start, end, lookup):
+    while start < end:
+        mid = (start + end) // 2
+        test_key = lookup(mid)
+        if test_key < key:
+            start = mid + 1
         else:
-            hi = mid
+            end = mid
 
-    start_idx = lo
-    lo = 0
-    hi = length
+    return start
 
-    while lo < hi:
-        mid = (lo + hi) // 2
-        key = register_lookup(mid)
-        if end_key < key:
-            hi = mid
+
+def bisect_right(key, start, end, lookup):
+    while start < end:
+        mid = (start + end) // 2
+        test_key = lookup(mid)
+        if key < test_key:
+            end = mid
         else:
-            lo = mid + 1
+            start = mid + 1
 
-    last_idx = lo - 1
-    return start_idx, last_idx
+    return start - 1
--- tests/unit/test_update.py
+++ tests/unit/test_update.py
@@ -1,72 +1,121 @@
+import pytest
+from collections import defaultdict
 from datetime import date
 from digital_land.register import Item
-from digital_land.collection import LogItem
+from digital_land.collection import SourceRegister, EndpointRegister, LogItem
 from digital_land.update import (
     has_collected_resource,
     get_failing_endpoints,
     get_entries_between_keys,
+    add_new_endpoint,
+    add_new_source,
 )
 
-LOG_ENTRIES = [
-    LogItem(
-        {
-            "endpoint": "AAA",
-            "url": "www.aaa.com",
-            "entry-date": "2018-06-25T13:41:49.222813",
-            "resource": "",
-            "status": "404",
-        }
-    ),
-    LogItem(
-        {
-            "endpoint": "BBB",
-            "url": "www.bbb.com",
-            "entry-date": "2019-05-10T13:41:49.222813",
-            "resource": "12345",
-            "status": "200",
-        }
-    ),
-    LogItem(
-        {
-            "endpoint": "CCC",
-            "url": "www.ccc.com",
-            "entry-date": "2020-04-15T13:41:49.222813",
-            "exception": "something failed",
-        }
-    ),
-    LogItem(
-        {
-            "endpoint": "CCC",
-            "url": "www.ccc.com",
-            "entry-date": "2020-05-20T13:41:49.222813",
-            "exception": "something failed",
-        }
-    ),
-    LogItem(
-        {
-            "endpoint": "DDD",
-            "url": "www.ddd.com",
-            "entry-date": "2020-10-20T13:41:49.222813",
-            "status": "200",
-        }
-    ),
-    LogItem(
-        {
-            "endpoint": "EEE",
-            "url": "www.eee.com",
-            "entry-date": "2020-10-25T13:41:49.222813",
-            "status": "500",
-        }
-    ),
-]
 
-ENDPOINT_ENTRIES = [
-    Item({"endpoint": "DDD", "end-date": "2020-10-21T13:41:49.222813"}),
-    Item({"endpoint": "AAA", "end-date": ""}),
-    Item({"endpoint": "BBB", "end-date": ""}),
-    Item({"endpoint": "CCC", "end-date": ""}),
-    Item({"endpoint": "EEE", "end-date": ""}),
-]
+@pytest.fixture()
+def log_entries():
+    return [
+        LogItem(
+            {
+                "endpoint": "AAA",
+                "url": "www.aaa.com",
+                "entry-date": "2018-06-25T13:41:49.222813",
+                "resource": "",
+                "status": "404",
+            }
+        ),
+        LogItem(
+            {
+                "endpoint": "BBB",
+                "url": "www.bbb.com",
+                "entry-date": "2019-05-10T13:41:49.222813",
+                "resource": "12345",
+                "status": "200",
+            }
+        ),
+        LogItem(
+            {
+                "endpoint": "CCC",
+                "url": "www.ccc.com",
+                "entry-date": "2020-04-15T13:41:49.222813",
+                "exception": "something failed",
+            }
+        ),
+        LogItem(
+            {
+                "endpoint": "CCC",
+                "url": "www.ccc.com",
+                "entry-date": "2020-05-20T13:41:49.222813",
+                "exception": "something failed",
+            }
+        ),
+        LogItem(
+            {
+                "endpoint": "DDD",
+                "url": "www.ddd.com",
+                "entry-date": "2020-10-20T13:41:49.222813",
+                "status": "200",
+            }
+        ),
+        LogItem(
+            {
+                "endpoint": "EEE",
+                "url": "www.eee.com",
+                "entry-date": "2020-10-25T13:41:49.222813",
+                "status": "500",
+            }
+        ),
+    ]
+
+
+@pytest.fixture()
+def endpoint_entries():
+    return [
+        Item({"endpoint": "AAA", "end-date": ""}),
+        Item({"endpoint": "BBB", "end-date": ""}),
+        Item({"endpoint": "CCC", "end-date": ""}),
+        Item({"endpoint": "DDD", "end-date": "2020-10-21T13:41:49.222813"}),
+        Item({"endpoint": "EEE", "end-date": ""}),
+    ]
+
+
+@pytest.fixture()
+def source_entries():
+    return [
+        Item({"organisation": "AZK", "endpoint": "AAA", "end-date": ""}),
+        Item(
+            {
+                "organisation": "BYT",
+                "endpoint": "BBB",
+                "end-date": "2020-10-21T13:41:49.222813",
+            }
+        ),
+        Item({"organisation": "CXJ", "endpoint": "CCC", "end-date": ""}),
+        Item(
+            {
+                "organisation": "CXJ",
+                "endpoint": "DDD",
+                "end-date": "2020-10-21T13:41:49.222813",
+            }
+        ),
+        Item({"organisation": "DWF", "endpoint": "BBB", "end-date": ""}),
+    ]
+
+
+@pytest.fixture()
+def source_register(source_entries):
+    register = SourceRegister()
+    for entry in source_entries:
+        register.add(entry)
+    return register
+
+
+@pytest.fixture()
+def endpoint_register(endpoint_entries):
+    register = EndpointRegister()
+    for entry in endpoint_entries:
+        register.add(entry)
+    return register
 
 
 def test_get_entries_between_keys():
@@ -87,34 +136,34 @@ def test_get_entries_between_keys():
     assert (start_idx, end_idx) == (2, 5)
 
 
-def test_has_collected_resource_404():
+def test_has_collected_resource_404(log_entries):
     # Return false due to 404
-    result, reason = has_collected_resource(LOG_ENTRIES[0].item)
+    result, reason = has_collected_resource(log_entries[0].item)
     assert result is False
 
 
-def test_has_collected_resource_exception():
+def test_has_collected_resource_exception(log_entries):
     # Return false due to exception
-    result, reason = has_collected_resource(LOG_ENTRIES[3].item)
+    result, reason = has_collected_resource(log_entries[3].item)
     assert result is False
 
 
-def test_has_collected_resource_no_resource():
+def test_has_collected_resource_no_resource(log_entries):
     # Return false due to missing resource
-    result, reason = has_collected_resource(LOG_ENTRIES[4].item)
+    result, reason = has_collected_resource(log_entries[4].item)
     assert result is False
 
 
-def test_has_collected_resource():
+def test_has_collected_resource(log_entries):
     # Return true
-    result, reason = has_collected_resource(LOG_ENTRIES[1].item)
+    result, reason = has_collected_resource(log_entries[1].item)
     assert result is True
 
 
-def test_get_failing_endpoints_filter_inactive():
+def test_get_failing_endpoints_filter_inactive(log_entries, endpoint_entries):
     # Test active endpoints
     failing_endpoints = get_failing_endpoints(
-        LOG_ENTRIES, ENDPOINT_ENTRIES, first_date=date(2017, 1, 1)
+        log_entries, endpoint_entries, first_date=date(2017, 1, 1)
     )
     expected_result = {"AAA", "CCC", "EEE"}
     assert (
@@ -123,10 +172,134 @@ def test_get_failing_endpoints_filter_inactive():
     )
 
 
-def test_get_failing_endpoints_filter_dates():
+def test_get_failing_endpoints_filter_dates(log_entries, endpoint_entries):
     # Test date filtering
     failing_endpoints = get_failing_endpoints(
-        LOG_ENTRIES, ENDPOINT_ENTRIES, first_date=date(2020, 10, 23)
+        log_entries, endpoint_entries, first_date=date(2020, 10, 23)
     )
     expected_result = {"EEE"}
     assert set(failing_endpoints.keys()) == expected_result
+
+
+def test_add_new_endpoint_order(endpoint_register):
+    expected_result = endpoint_register.entries.copy()
+    test_key = "BBC"
+    test_url = "www.someurl.com"
+    entry = defaultdict(str)
+    entry.update(
+        {
+            "endpoint-url": test_url,
+            "endpoint": test_key,
+            "organisation": "DWF",
+            "documentation-url": "www.doc.com",
+        }
+    )
+    add_new_endpoint(entry, endpoint_register)
+    assert expected_result == endpoint_register.entries[0:-1]
+    assert endpoint_register.entries[-1].item["endpoint"] == test_key
+    assert endpoint_register.entries[-1].item["endpoint-url"] == test_url
+
+
+def test_add_new_endpoint_existing_no_op(endpoint_register):
+    expected_result = endpoint_register.entries.copy()
+    entry = defaultdict(str)
+    entry.update(
+        {
+            "endpoint-url": "www.eee.com",
+            "endpoint": "EEE",
+            "organisation": "DWF",
+            "documentation-url": "www.doc.com",
+        }
+    )
+    add_new_endpoint(entry, endpoint_register)
+    assert endpoint_register.entries == expected_result
+
+
+def test_add_new_endpoint_existing_end_date(endpoint_register):
+    test_key = "DDD"
+    expected_result = endpoint_register.entries.copy()
+    entry = defaultdict(str)
+    entry.update(
+        {
+            "endpoint-url": "www.someurl.com",
+            "endpoint": test_key,
+            "organisation": "DWF",
+            "documentation-url": "www.doc.com",
+        }
+    )
+    add_new_endpoint(entry, endpoint_register)
+    assert expected_result == endpoint_register.entries[0:-1]
+    assert endpoint_register.entries[-1].item["endpoint"] == test_key
+
+
+def test_add_new_source(source_register):
+    expected_result = source_register.entries.copy()
+    entry = defaultdict(str)
+    entry.update(
+        {
+            "endpoint-url": "www.test.com",
+            "endpoint": "EEE",
+            "organisation": "CXJ",
+            "documentation-url": "www.doc.com",
+            "start-date": "",
+        }
+    )
+    add_new_source(entry, source_register)
+    assert expected_result == source_register.entries[0:-1]
+    assert source_register.entries[-1].item["endpoint"] == entry["endpoint"]
+    assert (
+        source_register.entries[-1].item["documentation-url"]
+        == entry["documentation-url"]
+    )
+    assert source_register.entries[-1].item["start-date"] == entry["start-date"]
+
+
+def test_add_new_source_existing_endpoint(source_register):
+    expected_result = source_register.entries.copy()
+    entry = defaultdict(str)
+    entry.update(
+        {
+            "endpoint-url": "www.test.com",
+            "endpoint": "BBB",
+            "organisation": "ERT",
+            "documentation-url": "www.doc.com",
+        }
+    )
+    add_new_source(entry, source_register)
+    assert expected_result == source_register.entries[0:-1]
+    assert source_register.entries[-1].item["endpoint"] == entry["endpoint"]
+    assert (
+        source_register.entries[-1].item["documentation-url"]
+        == entry["documentation-url"]
+    )
+
+
+def test_add_new_source_existing_no_op(source_register):
+    expected_result = source_register.entries.copy()
+    entry = defaultdict(str)
+    entry.update(
+        {
+            "endpoint-url": "www.test.com",
+            "endpoint": "BBB",
+            "organisation": "DWF",
+            "documentation-url": "www.doc.com",
+        }
+    )
+    add_new_source(entry, source_register)
+    assert source_register.entries == expected_result
+
+
+def test_add_new_source_existing_end_date(source_register):
+    expected_result = source_register.entries.copy()
+    entry = defaultdict(str)
+    entry.update(
+        {
+            "endpoint-url": "www.test.com",
+            "endpoint": "BBB",
+            "organisation": "BYT",
+            "documentation-url": "www.doc.com",
+        }
+    )
+    add_new_source(entry, source_register)
+    assert expected_result == source_register.entries[0:-1]
+    assert source_register.entries[-1].item["endpoint"] == entry["endpoint"]
