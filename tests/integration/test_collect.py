4c9a5443f601289e5815a3495a4f52eb6eff95ce
Kishan Patel <16578929+ksp37@users.noreply.github.com>
2020-11-20 14:34:15 +0000

    Updated tests.

--- tests/integration/test_collect.py
+++ tests/integration/test_collect.py
@@ -1,13 +1,17 @@
+import os
+import shutil
 import csv
 import urllib.request
 import json
 import hashlib
 from datetime import datetime
 from helpers import execute
+from pathlib import Path
 
 import pytest
 
 ENDPOINT = "https://www.registers.service.gov.uk/registers/country/download-csv"
+COLLECTION_DIR = "./collection"
 
 
 @pytest.fixture()
@@ -39,7 +43,7 @@ def test_collect(endpoint_csv):
     )
 
     log_date = datetime.utcnow().isoformat()[:10]
-    log_file = f"./collection/log/{log_date}/{hash_digest(ENDPOINT)}.json"
+    log_file = f"{COLLECTION_DIR}/log/{log_date}/{hash_digest(ENDPOINT)}.json"
 
     assert returncode == 0, f"return code non-zero: {errs}"
     assert "ERROR" not in errs
@@ -49,15 +53,20 @@ def test_collect(endpoint_csv):
     assert resource_collected(resource)
 
 
+def teardown_function():
+    if Path(COLLECTION_DIR).is_dir():
+        shutil.rmtree(Path(COLLECTION_DIR))
+
+
 def read_log(log_file):
     data = open(log_file).read()
     log = json.loads(data)
-    assert log["url"] == ENDPOINT
+    assert log["endpoint-url"] == ENDPOINT
     return log["resource"]
 
 
 def resource_collected(resource):
-    saved = open(f"./collection/resource/{resource}").read().rstrip()
+    saved = open(f"{COLLECTION_DIR}/resource/{resource}").read().rstrip()
     raw = urllib.request.urlopen(ENDPOINT).read().decode("utf-8")
     downloaded = "\n".join(raw.splitlines())  # Convert CRLF to LF
     return saved == downloaded
