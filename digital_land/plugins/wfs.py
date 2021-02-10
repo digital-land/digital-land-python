114d2a414504b7fd8b221ce6e357a238a317b822
Jon Bramley <brammers@gmail.com>
2021-02-10 10:39:35 +0000

    experiment: canonicalise xml for wfs endpoints

--- digital_land/plugins/wfs.py
+++ digital_land/plugins/wfs.py
@@ -1,7 +1,10 @@
 import io
 import re
-from digital_land.load import detect_encoding
+import xml.sax
 
+import lxml.etree as ET
+
+from digital_land.load import detect_encoding
 
 strip_exps = [
     (re.compile(br' ?timeStamp="[^"]*"'), br""),
@@ -16,9 +19,29 @@ def strip_variable_content(content):
     return content
 
 
+def is_xml(content):
+    try:
+        xml.sax.parseString(content, xml.sax.ContentHandler())
+        return True
+    except:  # SAX' exceptions are not public
+        pass
+    return False
+
+
+def canonicalise_xml(content):
+    et = ET.fromstring(content)
+    root = et.getroottree()
+    output = io.BytesIO()
+    __import__("pdb").set_trace()
+    root.write(output, method="c14n")
+    return output.getvalue()
+
+
 def get(collector, url, log={}, plugin="wfs"):
     log, content = collector.get(url=url, log=log, plugin=plugin)
     encoding = detect_encoding(io.BytesIO(content))
     if encoding:
+        if is_xml(content) and False:
+            content = canonicalise_xml(content)
         content = strip_variable_content(content)
     return log, content
