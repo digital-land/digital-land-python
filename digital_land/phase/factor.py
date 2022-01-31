4ce3e6cf3b18db3b7fc6040c9376a51046d4c7fc
Paul Downey <psd@whatfettle.com>
2022-01-31 10:10:44 +0000

    Missing module

--- digital_land/phase/factor.py
+++ digital_land/phase/factor.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python3
-
-#
-#  split entries into a series of facts
-#
-
 import hashlib
 from .phase import Phase
 
@@ -14,24 +8,15 @@ def fact_hash(entity, field, value):
 
 
 class FactorPhase(Phase):
+    """
+    add fact identifiers, filter out facts with a missing entity
+    """
+
     def process(self, reader):
         for stream_data in reader:
             row = stream_data["row"]
-            for field in self.fields:
-                entity = row["entity"]
-                value = row["value"]
-                fact = fact_hash(entity, field, value)
-                o = {
-                    "fact": fact,
-                    "entity": entity,
-                    "field": field,
-                    "value": value,
-                    "start-date": row["start-date"],
-                    "end-date": row["end-date"],
-                    "entry-date": row["entry-date"],
-                    "resource": stream_data["resource"],
-                    "row-number": stream_data["row-number"],
-                }
 
-                stream_data["row"] = o
-                yield stream_data
+            if row["entity"]:
+                row["fact"] = fact_hash(row["entity"], row["field"], row["value"])
+
+            yield stream_data
