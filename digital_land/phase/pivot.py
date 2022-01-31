853dcd31a9cad9d07a3e7aa4eff81a1e1e4abaef
Paul Downey <psd@whatfettle.com>
2022-01-31 11:23:07 +0000

    Pivot and reduce phases

---
+++ digital_land/phase/pivot.py
@@ -0,0 +1,30 @@
+from .phase import Phase
+
+
+class PivotPhase(Phase):
+    """
+    split entries into a series of facts
+    """
+
+    def process(self, reader):
+        for stream_data in reader:
+            row = stream_data["row"]
+            for field, value in row.items():
+
+                if field in ["entity"]:
+                    continue
+
+                stream_data["row"] = {
+                    # fact
+                    "fact": "",
+                    "entity": row.get("entity", ""),
+                    "field": field,
+                    "value": value,
+
+                    # entry
+                    "resource": stream_data["resource"],
+                    "row-number": stream_data["row-number"],
+                    "entry-date": row["entry-date"],
+                }
+
+                yield stream_data
---
+++ digital_land/phase/reduce.py
@@ -0,0 +1,24 @@
+import logging
+from .phase import Phase
+
+
+class ReducePhase(Phase):
+    """
+    reduce fields to just those specified for the dataset
+    """
+
+    def __init__(self, fields):
+        #self.fields = list(set(fields).union(set(("entity"))))
+        self.fields = list(set(fields + ["entity"]))
+        logging.debug(f"reducing fields to {self.fields}")
+
+    def process(self, reader):
+        for stream_data in reader:
+            row = stream_data["row"]
+
+            o = {}
+            for field in self.fields:
+                o[field] = row.get(field, "")
+
+            stream_data["row"] = o
+            yield stream_data
