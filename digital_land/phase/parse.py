ea2be79c87c66a399d87c421cffe236b18a30903
Paul Downey <psd@whatfettle.com>
2022-02-06 17:38:42 +0000

    Refactored parse stream block

--- digital_land/phase/parse.py
+++ digital_land/phase/parse.py
@@ -3,37 +3,17 @@ from .phase import Phase
 
 class ParsePhase(Phase):
     """
-    convert lines of text into rows of fields
+    convert stream lines of text into rows of fields
     """
 
     def __init__(self, dataset):
         self.dataset = dataset
         self.fieldnames = None
-        self.line_stream = None
-        self.line_number = 0
 
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        line = next(self.line_stream)
-        self.line_number += 1
-        data = {
-            "resource": line["resource"],
-            "dataset": self.dataset,
-            "row": dict(zip(self.fieldnames, line["line"])),
-            "line-number": self.line_number,
-        }
-        return data
-
-    def process(self, line_stream):
-        try:
-            stream_data = next(line_stream)
-        except StopIteration:
-            self.fieldnames = []
-            self.line_stream = iter(())
-            return self
-
-        self.fieldnames = stream_data["line"]
-        self.line_stream = line_stream
-        return self
+    def process(self, stream):
+        self.fieldnames = next(stream)
+        for block in stream:
+            block["dataset"] = self.dataset
+            block["fieldnames"]: self.fieldnames
+            block["row"] = dict(zip(self.fieldnames, block["line"]))
+            yield block
--- digital_land/stream.py
+++ digital_land/stream.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 
 class Stream:
-    def __init__(self, path, f=None, resource=None):
+    def __init__(self, path, f=None, resource=None, dataset=None):
         if not f:
             f = open(path, newline="")
 
@@ -13,21 +13,25 @@ class Stream:
         self.path = path
         self.f = f
         self.resource = resource
+        self.dataset = dataset
         self.line_number = 0
 
-    def csvstream(self, f):
-        for block in f:
-            yield block.replace("\0", "")
+    @staticmethod
+    def _reader(f):
+        for line in f:
+            yield line.replace("\0", "")
 
     def next(self):
-        for line in csv.reader(self.csvstream(self.f)):
+        for line in csv.reader(self._reader(self.f)):
             self.line_number = self.line_number + 1
 
             yield {
+                "dataset": self.dataset,
                 "path": self.path,
                 "resource": self.resource,
                 "line": line,
                 "line-number": self.line_number,
+                "row": {},
             }
 
     def __next__(self):
