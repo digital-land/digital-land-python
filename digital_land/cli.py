b481ae216f739935f97b30bf4648ab2a61dfcd49
Toby Petty <tobypetty1@gmail.com>
2022-04-01 15:30:10 +0100

    Remove erroneous additional argument to API.dataset_dump_cmd

--- digital_land/cli.py
+++ digital_land/cli.py
@@ -176,7 +176,7 @@ def dataset_create_cmd(input_paths, output_path, organisation_path):
 )
 @input_output_path
 def dataset_dump_cmd(input_path, output_path, hoisted_output_path, no_dump_hoisted):
-    API.dataset_dump_cmd(input_path, output_path, hoisted_output_path)
+    API.dataset_dump_cmd(input_path, output_path)
     if not no_dump_hoisted:
         API.dataset_dump_hoisted_cmd(input_path, output_path, hoisted_output_path)
 
