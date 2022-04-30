e12c880f195f8bd9789f710a655b5b3ba8d73c20
Paul Downey <psd@whatfettle.com>
2022-04-30 12:36:06 +0100

    Refactored WKT code and tests
    Prompted by not processing flipped northings and eastings

--- digital_land/datatype/point.py
+++ digital_land/datatype/point.py
@@ -1,87 +1,18 @@
-#
-#  normalise values by type defined in the schema
-#  -- output is valid according to the 2019 guidance
-#  -- log fixes as suggestions for the user to amend
-#
-
-import decimal
-from .decimal import DecimalDataType
-from .datatype import DataType
-
-from pyproj import Transformer
-
-# convert from OSGB to WGS84
-# https://epsg.io/27700
-# https://epsg.io/4326
-osgb_to_wgs84 = Transformer.from_crs(27700, 4326)
-
-# convert from OSM to WGS84
-# https://epsg.io/3857
-# https://epsg.io/4326
-osm_to_wgs84 = Transformer.from_crs(3857, 4326)
+from .wkt import WktDataType, parse_wkt
 
 
-def degrees_like(lon, lat):
-    return lat > -60.0 and lat < 60.0 and lon > -60.0 and lon < 60.0
-
-
-def easting_northing_like(lon, lat):
-    return lat > 1000.0 and lat < 1000000.0 and lon > 1000.0 and lon < 1000000.0
-
-
-def osm_like(lon, lat):
-    return lat > 6000000.0 and lat < 10000000.0 and lon > -80000.0 and lon < 260000.0
-
-
-def within_england(lon, lat):
-    return lon > -7.0 and lon < 2 and lat > 49.5 and lat < 56.0
-
-
-class PointDataType(DataType):
-    def __init__(self, precision=6):
-        self.decimal = DecimalDataType(precision=precision)
-
+class PointDataType(WktDataType):
     def normalise(self, values, default=["", ""], issues=None):
-
-        # assumes values have both been decimal normalised
-        if "" in values:
+        if not values or "" in values:
             return default
 
-        try:
-            (lon, lat) = [decimal.Decimal(value) for value in values]
-        except decimal.InvalidOperation:
-            if issues:
-                issues.log("Failed to convert point to decimal %s", values)
+        point = f"POINT ({values[0]} {values[1]})"
+        point = super().normalise(point, issues=issues)
+
+        if not point:
             return default
 
-        value = ",".join(values)
+        geometry, issue = parse_wkt(point)
+        x, y = geometry.coords[0]
 
-        if degrees_like(lon, lat):
-            if not within_england(lon, lat):
-                lon, lat = lat, lon
-                if not within_england(lon, lat):
-                    if issues:
-                        issues.log("WGS84 outside England", value)
-                    return default
-        elif easting_northing_like(lon, lat):
-            lat, lon = osgb_to_wgs84.transform(lon, lat)
-            if not within_england(lon, lat):
-                lat, lon = osgb_to_wgs84.transform(lat, lon)
-                if not within_england(lon, lat):
-                    if issues:
-                        issues.log("OSGB outside England", value)
-                    return default
-        elif osm_like(lon, lat):
-            lat, lon = osm_to_wgs84.transform(lon, lat)
-            if not within_england(lon, lat):
-                lat, lon = osm_to_wgs84.transform(lat, lon)
-                if not within_england(lon, lat):
-                    if issues:
-                        issues.log("OSM outside England", value)
-                    return default
-        else:
-            if issues:
-                issues.log("out of range", value)
-            return default
-
-        return [self.decimal.format(lon), self.decimal.format(lat)]
+        return [str(x), str(y)]
--- digital_land/datatype/wkt.py
+++ digital_land/datatype/wkt.py
@@ -1,86 +1,125 @@
 import shapely.wkt
 from shapely.ops import transform
 from shapely.geometry import MultiPolygon
-
+from pyproj import Transformer
 from .datatype import DataType
-from .point import (
-    degrees_like,
-    easting_northing_like,
-    osm_like,
-    osgb_to_wgs84,
-    osm_to_wgs84,
-    within_england,
-)
+
+
+# convert from OSGB Northings and Eastings to WGS84
+# https://epsg.io/27700
+# https://epsg.io/4326
+osgb_to_wgs84 = Transformer.from_crs(27700, 4326)
+
+# convert from Pseudo-Mercator metres to WGS84 decimal degrees
+# https://epsg.io/3857
+# https://epsg.io/4326
+# https://pyproj4.github.io/pyproj/stable/api/transformer.html#transformer
+metres_to_wgs84 = Transformer.from_crs(3857, 4326, always_xy=True)
+
+
+def degrees_like(x, y):
+    return x > -60.0 and x < 60.0 and y > -60.0 and y < 60.0
+
+
+def easting_northing_like(x, y):
+    return x > 1000.0 and x < 1000000.0 and y > 1000.0 and y < 1000000.0
+
+
+def metres_like(x, y):
+    return y > 6000000.0 and y < 10000000.0
+
+
+# bounding box check
+def within_england(x, y):
+    return x > -7.0 and x < 2 and y > 49.5 and y < 56.0
+
+
+# check if NW of the SE corner of the Irish Sea
+def osgb_within_england(x, y):
+    return not (x < 270000.0 and y > 400000.0)
 
 
 def flip(x, y, z=None):
     return tuple(filter(None, [y, x, z]))
 
 
+def parse_wkt(value):
+    # TBD: turn exception into issue
+    geometry = shapely.wkt.loads(value)
+
+    if geometry.geom_type == "Point":
+        first_point = geometry.coords[0]
+    else:
+        first_point = geometry.geoms[0].exterior.coords[0]
+
+    x, y = first_point[:2]
+
+    if degrees_like(x, y):
+        if within_england(x, y):
+            return geometry, None
+
+        if within_england(y, x):
+            return transform(flip, geometry), "WGS84 flipped"
+
+        return None, "WGS84 out of bounds"
+
+    if easting_northing_like(x, y):
+        if osgb_within_england(x, y):
+            return transform(osgb_to_wgs84.transform, geometry), "OSGB"
+
+        if osgb_within_england(y, x):
+            geometry = transform(flip, geometry)
+            geometry = transform(osgb_to_wgs84.transform, geometry)
+            geometry = transform(flip, geometry)
+            return geometry, "OSGB flipped"
+
+        return None, "OSGB out of bounds"
+
+    if metres_like(x, y):
+        _x, _y = metres_to_wgs84.transform(x, y)
+        if within_england(_x, _y):
+            return transform(metres_to_wgs84.transform, geometry), "Metres"
+
+    if metres_like(y, x):
+        _x, _y = metres_to_wgs84.transform(y, x)
+        if within_england(_x, _y):
+            geometry = transform(flip, geometry)
+            geometry = transform(metres_to_wgs84.transform, geometry)
+            return geometry, "Metres flipped"
+
+    return None, "invalid"
+
+
+def dump_wkt(geometry, precision=6, simplification=0.000005, dimensions=2):
+    if geometry.geom_type != "Point":
+
+        # see https://gist.github.com/psd/0189bc66fd46e00a82df2acbc7e35c8a
+        geometry = geometry.simplify(simplification)
+
+        # force geometry to be MULTIPOLYGON
+        if not isinstance(geometry, MultiPolygon):
+            geometry = MultiPolygon([geometry])
+
+    wkt = shapely.wkt.dumps(
+        geometry, rounding_precision=precision, output_dimension=dimensions
+    )
+    return wkt.replace(", ", ",")
+
+
 class WktDataType(DataType):
-    def __init__(self, precision=6):
+    def __init__(self):
         pass
 
     def normalise(self, value, default="", issues=None):
-        if value == "":
+        if not value:
             return default
 
-        geometry = shapely.wkt.loads(value)
+        geometry, issue = parse_wkt(value)
 
-        if geometry.geom_type == "Point":
-            first_point = geometry.coords[0]
-        else:
-            first_point = geometry.geoms[0].exterior.coords[0]
+        if issue:
+            issues.log(issue, "")
 
-        lon, lat = first_point[:2]
-        transposed_coords = False
-
-        if degrees_like(lon, lat):
-            if not within_england(lon, lat):
-                lon, lat = lat, lon
-                if within_england(lon, lat):
-                    transposed_coords = True
-                else:
-                    if issues:
-                        issues.log("WGS84 outside England", f"{value[0:30]}...")
-                    return default
-        elif easting_northing_like(lon, lat):
-            lat, lon = osgb_to_wgs84.transform(lon, lat)
-            if within_england(lon, lat):
-                transposed_coords = True
-            else:
-                lat, lon = osgb_to_wgs84.transform(lat, lon)
-                if not within_england(lon, lat):
-                    if issues:
-                        issues.log("OSGB outside England", f"{value[0:30]}...")
-                    return default
-            geometry = transform(osgb_to_wgs84.transform, geometry)
-        elif osm_like(lon, lat):
-            lat, lon = osm_to_wgs84.transform(lon, lat)
-            if within_england(lon, lat):
-                transposed_coords = True
-            else:
-                lat, lon = osm_to_wgs84.transform(lat, lon)
-                if not within_england(lon, lat):
-                    if issues:
-                        issues.log("OSM outside England", f"{value[0:30]}...")
-                    return default
-            geometry = transform(osm_to_wgs84.transform, geometry)
-        else:
-            if issues:
-                issues.log("out of range", f"{value[0:30]}...")
+        if not geometry:
             return default
 
-        if transposed_coords:
-            geometry = transform(flip, geometry)
-
-        if geometry.geom_type != "Point":
-            # see https://gist.github.com/psd/0189bc66fd46e00a82df2acbc7e35c8a
-            geometry = geometry.simplify(0.000005)
-            if not isinstance(geometry, MultiPolygon):
-                # simplify will reduce to simple Polygon if possible
-                geometry = MultiPolygon([geometry])
-
-        return shapely.wkt.dumps(
-            geometry, rounding_precision=6, output_dimension=2
-        ).replace(", ", ",")
+        return dump_wkt(geometry)
