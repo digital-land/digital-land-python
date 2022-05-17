07995eca1bea1f783fe65c5a71affba7effd3c97
Paul Downey <psd@whatfettle.com>
2022-05-17 08:04:09 +0100

    Black fixed quoting issue

--- digital_land/plugins/wfs.py
+++ digital_land/plugins/wfs.py
@@ -4,14 +4,11 @@ import re
 from digital_land.phase.convert import detect_encoding
 
 
-# Black (22.1.0) doesn't recognize that raw strings containing " have to be enclosed in '
-# fmt: off
 strip_exps = [
     (re.compile(rb' ?timeStamp="[^"]*"'), rb""),
     (re.compile(rb' ?fid="[^"]*"'), rb""),
     (re.compile(rb'(gml:id="[^."]+)[^"]*'), rb"\1"),
 ]
-# fmt: on
 
 
 def strip_variable_content(content):
