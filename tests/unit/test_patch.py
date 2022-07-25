82a4011362fbf45b359e9cd549b0aab8a6682037
Paul Downey <psd@whatfettle.com>
2022-07-25 14:31:43 +0100

    Made patch tests more realistic

--- tests/unit/test_patch.py
+++ tests/unit/test_patch.py
@@ -4,19 +4,39 @@ from digital_land.phase.patch import PatchPhase
 from digital_land.log import IssueLog
 
 
-def test_patch_phase():
+def test_patch_regex():
     issues = IssueLog()
 
-    patches = {"field-string": {"WRONG": "right", "same": "same"}}
+    patches = {"grade": {"^1$": "I", "^2$": "II", "^2\\*$": "II*", "^2 Star$": "II*", "^3$": "III"}}
 
     p = PatchPhase(patches=patches, issues=issues)
 
-    assert p.apply_patch("field-string", "right") == "right"
-    assert p.apply_patch("field-string", "WRONG") == "right"
-    assert p.apply_patch("field-string", "same") == "same"
+    assert p.apply_patch("grade", "II") == "II"
+    assert issues.rows == []
+
+    assert p.apply_patch("grade", "II*") == "II*"
+    assert issues.rows == []
+
+    assert p.apply_patch("grade", "2") == "II"
 
     issue = issues.rows.pop()
-    assert issue["field"] == "field-string"
+    assert issue["field"] == "grade"
     assert issue["issue-type"] == "patch"
-    assert issue["value"] == "WRONG"
+    assert issue["value"] == "2"
+    assert issues.rows == []
+
+    assert p.apply_patch("grade", "2*") == "II*"
+
+    issue = issues.rows.pop()
+    assert issue["field"] == "grade"
+    assert issue["issue-type"] == "patch"
+    assert issue["value"] == "2*"
+    assert issues.rows == []
+
+    assert p.apply_patch("grade", "2 Star") == "II*"
+
+    issue = issues.rows.pop()
+    assert issue["field"] == "grade"
+    assert issue["issue-type"] == "patch"
+    assert issue["value"] == "2 Star"
     assert issues.rows == []
