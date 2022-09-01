aa1366e46695c00b1c8b44c6c4da22ba8dc45ff8
Adam Shimali <adam.shimali@gmail.com>
2022-09-01 10:36:58 +0100

    Incorrect test file name

--- tests/integration/test_package_dataset.py
+++ tests/integration/test_package_dataset.py
@@ -65,7 +65,7 @@ def test_package_dataset(
 
 @pytest.mark.parametrize(
     "dataset_name",
-    ["listed-building-outline"],
+    ["listed-building-grade"],
 )
 def test_package_dataset_hoisted(
     # Parametrize args
