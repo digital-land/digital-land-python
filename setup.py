59f7caf8ed7b0db0c983d3fb8c8dddce58f0ff10
Adam Shimali <adam.shimali@gmail.com>
2022-11-15 10:09:08 +0000

    datasette required to create inspect file

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
     package_data={"digital-land": ["templates/*.html"]},
     include_package_data=True,
     install_requires=[
+        "datasette",
         "canonicaljson",
         "click",
         "cchardet",
