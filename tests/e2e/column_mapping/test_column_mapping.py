ae2c06841da0632f128aff95e753695450e9e026
morgan-sl <128702799+morgan-sl@users.noreply.github.com>
2023-05-18 19:35:45 +0100

    Added first draft of test_pipeline_run. Not optimised to use common data directories

--- tests/e2e/column_mapping/test_column_mapping.py
+++ tests/e2e/column_mapping/test_column_mapping.py
@@ -7,7 +7,9 @@ import pytest
 from pathlib import Path
 from urllib.error import URLError
 
-from digital_land.log import ColumnFieldLog
+from digital_land.commands import pipeline_run
+from digital_land.log import ColumnFieldLog, IssueLog
+from digital_land.organisation import Organisation
 from digital_land.phase.map import MapPhase
 from digital_land.pipeline import Pipeline, run_pipeline
 from digital_land.specification import Specification
@@ -15,6 +17,56 @@ from digital_land.specification import Specification
 from io import StringIO
 from digital_land.phase.load import LoadPhase
 from digital_land.phase.parse import ParsePhase
+
+
+test_pipeline = "national-park"
+test_resource = "5158d13bfc6f0723b1fb07c975701a906e83a1ead4aee598ee34e241c79a5f3d"
+test_endpoint = "d779ad1c91c5a46e2d4ace4d5446d7d7f81df1ed058f882121070574697a5412"
+
+
+@pytest.fixture(scope="session")
+def test_dirs(tmp_path_factory):
+    """
+    This fixture prepares the folders and data required by the tests that use it.
+    Some of the data is retrieved remotely, so it makes sense to retrieve once only
+    for all tests.
+    :param tmp_path_factory:
+    :return: dict of Path (directory paths)
+    """
+
+    pipeline_dir = tmp_path_factory.mktemp("pipeline", numbered=False)
+
+    specification_dir = tmp_path_factory.mktemp("specification", numbered=False)
+
+    collection_dir = tmp_path_factory.mktemp("collection", numbered=False)
+
+    output_dir = tmp_path_factory.mktemp("output", numbered=False)
+
+    # data - pipeline
+    raw_data = get_pipeline_csv_data_with_resources_and_endpoints(
+        test_pipeline, test_resource, test_endpoint
+    )
+    columns_data = pd.DataFrame.from_dict(raw_data)
+    columns_data.to_csv(f"{pipeline_dir}/column.csv", index=False)
+
+    # data - specification
+    copy_latest_specification_files_to(specification_dir)
+
+    current_fields_df = pd.read_csv(specification_dir / "field.csv")
+    additional_fields_dict = get_additional_field_csv_data(specification_dir)
+    additional_fields_df = pd.DataFrame(additional_fields_dict)
+    fields_df = pd.concat([additional_fields_df, current_fields_df], ignore_index=True)
+    fields_df.to_csv(specification_dir / "field.csv", index=False)
+
+    # data - collection
+    generate_test_collection_files(str(collection_dir), test_pipeline, test_resource, test_endpoint)
+
+    return {
+        "pipeline_dir": pipeline_dir,
+        "specification_dir": specification_dir,
+        "collection_dir": collection_dir,
+        "output_dir": output_dir,
+    }
 
 
 def get_pipeline_csv_data_with_resources_and_endpoints(
@@ -90,6 +142,96 @@ def copy_latest_specification_files_to(specification_dir: Path):
         pytest.fail(error_msg)
 
 
+def get_additional_field_csv_data(specification_dir: Path = None):
+    return {
+        "cardinality": [1, 1],
+        "datatype": ["string", "string"],
+        "description": ["resource field map", "endpoint field map"],
+        "end-date": ["", ""],
+        "entry-date": ["", ""],
+        "field": ["res_field_one", "ep_field_one"],
+        "guidance": ["", ""],
+        "hint": ["", ""],
+        "name": ["res_field_one", "ep_field_one"],
+        "parent-field": ["", ""],
+        "replacement-field": ["", ""],
+        "start-date": ["", ""],
+        "text": ["value", "value"],
+        "typology": ["", ""],
+        "uri-template": ["", ""],
+        "wikidata-property": ["", ""]
+    }
+
+
+def generate_test_collection_files(
+        collection_dir: str = "", pipeline: str = "", resource: str = "", endpoint: str = ""
+):
+    error_msg = "Failed to generate collection files"
+
+    collection_csv_list = [
+        "endpoint.csv",
+        "source.csv",
+        "resource.csv",
+        f"{resource}.csv",
+    ]
+
+    try:
+        for collection_csv in collection_csv_list:
+            if collection_csv == "endpoint.csv":
+                raw_data = {
+                    "endpoint": [endpoint],
+                    "endpoint-url": ["https://example.com/register/1.csv"],
+                    "start-date": ["2020-01-12"],
+                    "end-date": ["2020-02-01"],
+                }
+
+            if collection_csv == "source.csv":
+                raw_data = {
+                    "source": [""],
+                    "collection": ["test-collection"],
+                    "pipeline": [pipeline],
+                    "organisation": ["test-org"],
+                    "endpoint": [endpoint],
+                    "documentation-url": [""],
+                    "licence": [""],
+                    "attribution": [""],
+                    "start-date": [""],
+                    "end-date": [""],
+                }
+
+            if collection_csv == "resource.csv":
+                raw_data = {
+                    "resource": [resource],
+                    "bytes": [1234],
+                    "organisations": ["test-org"],
+                    "datasets": [pipeline],
+                    "endpoints": [endpoint],
+                    "res-col-one": ["res-col-val"],
+                    "ep-col-one": ["ep-col-val"],
+                    "start-date": ["2020-01-12"],
+                    "end-date": ["2020-02-01"],
+                }
+
+            if collection_csv.startswith(resource):
+                raw_data = {
+                    "resource": [resource],
+                    "bytes": [1234],
+                    "organisations": ["test-org"],
+                    "datasets": [pipeline],
+                    "endpoints": [endpoint],
+                    "res-col-one": ["res-col-val"],
+                    "ep-col-one": ["ep-col-val"],
+                    "start-date": ["2020-01-12"],
+                    "end-date": ["2020-02-01"],
+                }
+
+            columns_data = pd.DataFrame.from_dict(raw_data)
+            columns_data.to_csv(f"{collection_dir}/{collection_csv}", index=False)
+
+    except URLError:
+        pytest.fail(error_msg)
+
+
 def create_inputs_stream_from_dict(inputs: dict):
     separator = ","
     keys_str_list = separator.join(list(inputs.keys()))
@@ -107,6 +249,11 @@ def apply_pipeline_transforms(phases: list):
     run_pipeline(*phases)
 
 
+# def test_one(test_dirs):
+#     print("")
+#     print(f">>> test_two: {test_dirs}")
+
+
 def test_pipeline_with_map_phase(tmp_path):
     # -- Arrange --
     pipeline_dir = tmp_path / "pipeline"
@@ -118,9 +265,9 @@ def test_pipeline_with_map_phase(tmp_path):
     output_dir = tmp_path / "output"
     output_dir.mkdir()
 
-    test_pipeline = "national-park"
-    test_resource = "5158d13bfc6f0723b1fb07c975701a906e83a1ead4aee598ee34e241c79a5f3d"
-    test_endpoint = "d779ad1c91c5a46e2d4ace4d5446d7d7f81df1ed058f882121070574697a5412"
+    # test_pipeline = "national-park"
+    # test_resource = "5158d13bfc6f0723b1fb07c975701a906e83a1ead4aee598ee34e241c79a5f3d"
+    # test_endpoint = "d779ad1c91c5a46e2d4ace4d5446d7d7f81df1ed058f882121070574697a5412"
 
     dummy_endpoint = "abcde12345fghij67890abcde12345fghij67890abcde12345fghij67890abcd"
     test_endpoints = [dummy_endpoint, test_endpoint, dummy_endpoint[::-1]]
@@ -177,3 +324,67 @@ def test_pipeline_with_map_phase(tmp_path):
     assert "ep_field_one" in df["field"].values
     assert "name" in df["field"].values
     assert "res_field_one" in df["field"].values
+
+
+def test_pipeline_run(test_dirs):
+    """
+    This test uses commands.py pipeline_run to perform a full run-through
+    of the full pipeline phases, using test data generated by the input
+    fixture test_dirs, which itself uses the global vars:
+    test_pipeline
+    test_resource
+    test_endpoint
+    :param test_dirs:
+    :return: None
+    """
+    # -- Arrange --
+    dummy_endpoint = "abcde12345fghij67890abcde12345fghij67890abcde12345fghij67890abcd"
+
+    pipeline_dir = test_dirs["pipeline_dir"]
+    specification_dir = test_dirs["specification_dir"]
+
+    pipeline = Pipeline(pipeline_dir, test_pipeline)
+    specification = Specification(specification_dir)
+    input_path = test_dirs["collection_dir"] / f"{test_resource}.csv"
+    output_path = test_dirs["pipeline_dir"] / f"{test_resource}.csv"
+    collection_dir = test_dirs["collection_dir"]
+    issue_dir = test_dirs["collection_dir"]
+    organisation_path = "tests/data/listed-building/organisation.csv"
+    dataset_resource_dir = test_dirs["collection_dir"]
+    test_endpoints = [dummy_endpoint, test_endpoint, dummy_endpoint[::-1]]
+
+    # -- Act --
+    pipeline_run(
+        dataset=test_pipeline,
+        pipeline=pipeline,
+        specification=specification,
+        input_path=input_path,
+        output_path=output_path,
+        collection_dir=collection_dir,  # TBD: remove, replaced by endpoints, organisations and entry_date
+        null_path=None,  # TBD: remove this
+        issue_dir=issue_dir,
+        organisation_path=organisation_path,
+        save_harmonised=False,
+        column_field_dir=pipeline_dir,
+        dataset_resource_dir=dataset_resource_dir,
+        custom_temp_dir=None,  # TBD: rename to "tmpdir"
+        endpoints=test_endpoints,
+        organisations=[],
+        entry_date=""
+    )
+
+    # -- Asert --
+    output_file = output_path
+    df = pd.read_csv(output_file, index_col=False)
+
+    assert "end-date" in df["column"].values
+    assert "ep-col-one" in df["column"].values
+    assert "res-col-one" in df["column"].values
+    assert "start-date" in df["column"].values
+
+    assert "end-date" in df["field"].values
+    assert "ep_field_one" in df["field"].values
+    assert "res_field_one" in df["field"].values
+    assert "start-date" in df["column"].values
+
+
