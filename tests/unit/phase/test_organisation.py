57dc0fffc813a1dfd5e4ebcfbedcc00af5778b2a
Samriti Sadhu <ssadhu@scottlogic.com>
2023-09-13 17:02:59 +0100

    Using Mocker for Organisation

--- tests/unit/phase/test_organisation.py
+++ tests/unit/phase/test_organisation.py
@@ -1,12 +1,16 @@
-from digital_land.organisation import Organisation
 from digital_land.phase.organisation import OrganisationPhase
 from digital_land.log import IssueLog
+from unittest.mock import Mock
 
 
-def test_process_with_valid_organisation():
-    organisation = Organisation(
-        organisation_path="tests/data/listed-building/organisation.csv"
-    )
+def test_process_with_valid_organisation(mocker):
+    mock_organisation = Mock()
+
+    # Mock the `lookup` method to return a predefined value
+    mock_organisation.lookup.return_value = "local-authority-eng:LBH"
+
+    mocker.patch("digital_land.phase.organisation", return_value=mock_organisation)
+
     input_stream = [
         {
             "row": {
@@ -15,15 +19,20 @@ def test_process_with_valid_organisation():
         }
     ]
 
-    organisationPhase = OrganisationPhase(organisation=organisation, issues=None)
+    organisationPhase = OrganisationPhase(organisation=mock_organisation, issues=None)
     output = [block for block in organisationPhase.process(input_stream)]
+    assert not organisationPhase.issues
     assert output[0]["row"]["organisation"] == "local-authority-eng:LBH"
 
 
-def test_process_with_invalid_organisation():
-    organisation = Organisation(
-        organisation_path="tests/data/listed-building/organisation.csv"
-    )
+def test_process_with_invalid_organisation(mocker):
+    mock_organisation = Mock()
+
+    # Mock the `lookup` method to return a predefined value
+    mock_organisation.lookup.return_value = ""
+
+    mocker.patch("digital_land.phase.organisation", return_value=mock_organisation)
+
     issues = IssueLog(dataset="brownfield-land", resource="1")
     input_stream = [
         {
@@ -35,6 +44,7 @@ def test_process_with_invalid_organisation():
             "entry-number": 2,
         }
     ]
-    organisationPhase = OrganisationPhase(organisation=organisation, issues=issues)
+    organisationPhase = OrganisationPhase(organisation=mock_organisation, issues=issues)
     output = [block for block in organisationPhase.process(input_stream)]
+    assert organisationPhase.issues
     assert output[0]["row"]["organisation"] == ""
