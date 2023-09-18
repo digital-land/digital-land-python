923a136cbd1bde7287d3778b009e47134f1600c3
morgan-sl <128702799+morgan-sl@users.noreply.github.com>
2023-09-18 16:50:13 +0100

    Refinement VIII: applied amendments based on PR review 3.

--- tests/unit/test_pipeline.py
+++ tests/unit/test_pipeline.py
@@ -149,38 +149,32 @@ def test_lookups_validate_entry_success():
     assert actual_result == expected_result
 
 
-def test_lookups_validate_entry_failure():
+@pytest.mark.parametrize(
+    "entry",
+    [
+        {},
+        {"prefix": ""},
+        {"prefix": "", "organisation": ""},
+        {"prefix": "", "organisation": "", "reference": ""},
+        {"prefix": "", "organisation": "", "reference": "", "entity": ""},
+        {
+            "prefix": "",
+            "organisation": "",
+            "reference": "",
+            "entity": "",
+            "resource": "",
+        },
+    ],
+)
+def test_lookups_validate_entry_failure(entry):
     """
     test csv validate_entry functionality for various errors
     :return:
     """
     lookups = Lookups("")
-    invalid_entries = [
-        {},
-        {
-            "prefix": "",
-        },
-        {
-            "prefix": "",
-            "organisation": "",
-        },
-        {
-            "prefix": "",
-            "organisation": "",
-            "reference": "",
-        },
-        {
-            "prefix": "",
-            "resource": "",
-            "organisation": "",
-            "reference": "",
-            "entity": "",
-        },
-    ]
 
-    for entry in invalid_entries:
-        with pytest.raises(ValueError):
-            lookups.validate_entry(entry)
+    with pytest.raises(ValueError):
+        lookups.validate_entry(entry)
 
     expected_length = 0
     assert len(lookups.entries) == expected_length
