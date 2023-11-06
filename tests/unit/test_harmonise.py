1e1d9f61aa00a11aa3f9d61afaa2b5137c044628
CarlosCoelhoSL <ccoelho@scottlogic.com>
2023-11-06 13:38:29 +0000

    Removes failing test

--- tests/unit/test_harmonise.py
+++ tests/unit/test_harmonise.py
@@ -193,8 +193,3 @@ def test_harmonise_outside_default_boundary_unknown_organisation():
     assert len(output) == 1
     assert output[0]["row"]["GeoX"] == ""
     assert output[0]["row"]["GeoY"] == ""
-
-
-def test_failing_test():
-    variable = "fail"
-    assert variable == "pass"
