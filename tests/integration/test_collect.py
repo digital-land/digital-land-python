f6d3499f47ad11c21cb9b9c588526b01295483d2
Samriti Sadhu <ssadhu@scottlogic.com>
2024-01-30 10:05:17 +0000

    Revert "Testing test_collect deletion"
    This reverts commit 84ac4c669a1cba34ac0ecf5d3014711987492a59.

---
+++ tests/integration/test_collect.py
@@ -0,0 +1,66 @@
+import csv
+import json
+import urllib.request
+import pytest
+
+from datetime import datetime
+from click.testing import CliRunner
+from digital_land.cli import cli
+
+from tests.utils.helpers import hash_digest
+
+ENDPOINT = "https://raw.githubusercontent.com/digital-land/digital-land-python/main/tests/data/resource_examples/csv.csv"
+COLLECTION_DIR = "./collection"
+
+
+@pytest.fixture()
+def endpoint_csv(tmp_path):
+    p = tmp_path / "endpoint.csv"
+    fieldnames = ["endpoint", "endpoint-url"]
+    with open(p, "w") as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerow({"endpoint": hash_digest(ENDPOINT), "endpoint-url": ENDPOINT})
+    return str(p)
+
+
+@pytest.fixture()
+def collection_dir(tmp_path):
+    c = tmp_path / "collection"
+    c.mkdir()
+    return str(c)
+
+
+def test_collect(endpoint_csv, collection_dir):
+    args = [
+        "collect",
+        "--collection-dir",
+        collection_dir,
+        endpoint_csv,
+    ]
+
+    runner = CliRunner()
+    result = runner.invoke(cli, args)
+
+    log_date = datetime.utcnow().isoformat()[:10]
+    log_file = f"{collection_dir}/log/{log_date}/{hash_digest(ENDPOINT)}.json"
+
+    assert 0 == result.exit_code
+
+    resource = read_log(log_file)
+    assert resource
+    assert resource_collected(collection_dir, resource)
+
+
+def read_log(log_file):
+    data = open(log_file).read()
+    log = json.loads(data)
+    assert log["endpoint-url"] == ENDPOINT
+    return log["resource"]
+
+
+def resource_collected(collection_dir, resource):
+    saved = open(f"{collection_dir}/resource/{resource}").read().rstrip()
+    raw = urllib.request.urlopen(ENDPOINT).read().decode("utf-8")
+    downloaded = "\n".join(raw.splitlines())  # Convert CRLF to LF
+    return saved == downloaded
