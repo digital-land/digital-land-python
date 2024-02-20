6a70e47fb5dd02be0428cc22cfb36aec5c2512c2
Samriti Sadhu <ssadhu@scottlogic.com>
2024-02-20 17:38:14 +0000

    adding return statement

--- digital_land/datatype/multipolygon.py
+++ digital_land/datatype/multipolygon.py
@@ -3,4 +3,4 @@ from .wkt import WktDataType
 
 class MultiPolygon(WktDataType):
     def normalise(self, values, issues=None):
-        super().normalise(values, issues=issues)
+        return super().normalise(values, issues=issues)
