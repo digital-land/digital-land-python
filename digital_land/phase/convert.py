c5c8b0cbd8c6e981b3efa1ff6b8e93bc0bd10432
Paul Downey <psd@whatfettle.com>
2024-06-13 18:09:56 +0100

    Assert WKT name broken by fixing the ogr2ogr command

--- digital_land/phase/convert.py
+++ digital_land/phase/convert.py
@@ -99,6 +99,8 @@ def convert_features_to_csv(input_path, output_path=None):
             "-lco",
             "GEOMETRY=AS_WKT",
             "-lco",
+            "GEOMETRY_NAME=WKT",
+            "-lco",
             "LINEFORMAT=CRLF",
             "-f",
             "CSV",
