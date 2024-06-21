e9caa9f4c1b06734e87cf13e371feaca5b8f76ad
Christopher Johns <cjohns@scottlogic.com>
2024-06-21 09:36:32 +0100

    Updated test

--- tests/unit/test_prune.py
+++ tests/unit/test_prune.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env -S pytest -s
 
 from io import StringIO
+import csv
 from digital_land.phase.phase import Phase
 from digital_land.pipeline import run_pipeline
 from digital_land.phase.save import SavePhase
 from digital_land.phase.prune import FactPrunePhase
 
-test_data = [{"row": {"value": "42"}}, {"row": {"value": ""}}, {"row": {"value": None}}]
+test_data = [
+    {"row": {"value": "42", "end-date": "1999-12-31"}},
+    {"row": {"value": "", "end-date": "1999-12-31"}},
+    {"row": {"value": "42", "end-date": ""}},
+    {"row": {"value": "", "end-date": ""}},
+]
+
+expected_output = [
+    {"value": "42", "end-date": "1999-12-31"},
+    {"end-date": "1999-12-31"},
+    {"value": "42", "end-date": ""},
+    {"": ""},
+]
 
 
 class TestPhase(Phase):
-    def __init__(self):
-        pass
-
     def process(self, _):
         for block in test_data:
             yield block
@@ -26,4 +36,10 @@ def TestPrunePipeline():
 
 def test_parse_pipeline():
     output = TestPrunePipeline()
-    assert output.split() == ["value", "42", '""']
+    f = StringIO(output)
+    index = 0
+    for row in csv.DictReader(f, delimiter=","):
+        assert row == expected_output[index]
+        index += 1
+
+    assert index == len(expected_output)
