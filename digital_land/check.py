dbb020730b4067b672c6a0b76fd74750f5676296
CarlosCoelhoSL <ccoelho@scottlogic.com>
2024-07-19 10:14:48 +0100

    Adds sql query to duplicate ref check

--- digital_land/check.py
+++ digital_land/check.py
@@ -3,21 +3,36 @@ import duckdb
 
 def duplicate_reference_check(issues=None, csv_path=None):
     # csv_path = "test.csv"
-    duckdb.read_csv(csv_path)
-    sql = f"""
-    SELECT *
-    FROM '{csv_path}'
-    """
-    result = duckdb.sql(sql).df()
-    print(result.head(5))
+    try:
+        duckdb.read_csv(csv_path)
+        sql = f"""
+        SELECT
+            "field",
+            "value",
+            "entry-date",
+            COUNT(*) AS count,
+            STRING_AGG("entry-number", ',') AS entry_numbers
+        FROM read_csv_auto('{csv_path}')
+        WHERE "field" IN ('reference')
+        GROUP BY "field", "value", "entry-date";
+        """
+        count_table = duckdb.sql(sql).df()
+        duplicate_references = count_table[count_table["count"] > 1]
+        for idx, row in duplicate_references.iterrows():
+            for entry_number in row["entry_numbers"].split(","):
+                issues.log_issue(
+                    "reference",
+                    "duplicate reference",
+                    row["reference"],
+                    entry_number=entry_number,
+                    message="Reference must be unique in resource",
+                )
+                print(
+                    "issue for reference",
+                    row["reference"],
+                    "for entry number",
+                    entry_number,
+                )
+    except Exception as e:
+        print(e)
     return issues
-    # if len(result) > 1:
-    #     duplicate_references = result["references"].tolist()
-    #     for reference in duplicate_references:
-    #         self.issues.log(
-    #             "duplicate reference",
-    #             reference,
-    #             "There are multiple entries in the resource with the same reference",
-    #         )
-
-    # print("duplicate references:", duplicate_references)
