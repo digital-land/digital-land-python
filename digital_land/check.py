565b01300414200e3f6788742274f7864f4cc494
Samriti Sadhu <ssadhu@scottlogic.com>
2024-07-29 17:00:37 +0100

    Adding dask dataframe to handle large complex CSVs

--- digital_land/check.py
+++ digital_land/check.py
@@ -1,6 +1,6 @@
 import duckdb
 import logging
-import pandas as pd
+import dask.dataframe as dd
 
 
 # TODO This might need to move into expectations as it is a form of data checking
@@ -8,49 +8,16 @@ def duplicate_reference_check(issues=None, csv_path=None):
     try:
         conn = duckdb.connect()
 
+        ddf = dd.read_csv(csv_path)
+        ddf.columns = ddf.columns.str.replace("-", "_")
+        filtered_ddf = ddf[ddf["field"] == "reference"]
+
+        filtered_df = filtered_ddf.compute()
+        filtered_df[filtered_df["field"] == "reference"]
+        conn.execute("CREATE TABLE filtered_table AS SELECT * FROM filtered_df")
         conn.execute(
-            """
-        CREATE TABLE IF NOT EXISTS temp_table (
-            "field" VARCHAR,
-            "value" VARCHAR,
-            "entry_date" VARCHAR,
-            "entry_number" VARCHAR
-        );
-        """
+            "CREATE INDEX IF NOT EXISTS idx_field_value_date ON filtered_table(field, value, entry_date);"
         )
-        conn.execute(
-            "CREATE INDEX IF NOT EXISTS idx_field_value_date ON temp_table(field, value, entry_date);"
-        )
-
-        # Read the entire CSV file into a DataFrame
-        df = pd.read_csv(csv_path)
-
-        # Rename columns to match SQL naming conventions
-        df.columns = df.columns.str.replace("-", "_")
-        df = df.rename(
-            columns={
-                "field": "field",
-                "value": "value",
-                "entry_date": "entry_date",
-                "entry_number": "entry_number",
-            }
-        )
-
-        # Filter the DataFrame to include only rows where "field" is 'reference'
-        filtered_df = df[df["field"] == "reference"]
-
-        # Register the DataFrame with DuckDB
-        conn.register("filtered_df", filtered_df)
-
-        # Insert the filtered data into the DuckDB table
-        conn.execute(
-            """
-            INSERT INTO temp_table ("field", "value", "entry_date", "entry_number")
-            SELECT "field", "value", "entry_date", "entry_number"
-            FROM filtered_df;
-        """
-        )
-
         # SQL query to identify duplicate references
         sql = """
         SELECT
@@ -59,7 +26,7 @@ def duplicate_reference_check(issues=None, csv_path=None):
             "entry_date",
             COUNT(*) AS count,
             STRING_AGG("entry_number"::TEXT, ',') AS entry_numbers
-        FROM temp_table
+        FROM filtered_table
         GROUP BY "field", "value", "entry_date"
         HAVING COUNT(*) > 1;
         """
@@ -83,6 +50,6 @@ def duplicate_reference_check(issues=None, csv_path=None):
         logging.error("Duplicate reference check: Failed for %s" % (csv_path))
         logging.error(e)
     finally:
-        conn.execute("DROP TABLE IF EXISTS temp_table;")
+        conn.execute("DROP TABLE IF EXISTS filtered_table;")
         conn.close()
     return issues
