8c1ddae1c784829d914cb636aa05b96248866120
eveleighoj <35256612+eveleighoj@users.noreply.github.com>
2024-09-23 08:45:40 +0100

    add pg package  file

---
+++ digital_land/package/dataset_parquet.py
@@ -0,0 +1,17 @@
+# from .package import Package
+
+# class DatasetParquet(Package):
+#     """
+#     Up until now we have primarily been storing and loading datasets
+#     as sqllite. This package represents the same data but in a set
+#     of parquet files.
+#     """
+
+#     def load(self,transformed_resource_dir):
+#         """load data into the package, will replace the """
+
+#         load_fact_resources()
+#         load_facts
+#         load_entity
+
+#     def update()
