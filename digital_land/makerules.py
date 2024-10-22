c621b96585e817b5ccab97830c964400fee3183e
eveleighoj <35256612+eveleighoj@users.noreply.github.com>
2024-10-22 20:07:15 +0100

    move old_resource logic

--- digital_land/makerules.py
+++ digital_land/makerules.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
 
-import os
-from pathlib import Path
-
-
 #
 #  create the dependencies between collected resources and a dataset
 #  saved as collection/pipeline.mk
@@ -20,6 +16,9 @@ def dataset_path(dataset):
 
 def pipeline_makerules(collection):
     dataset_resource = collection.dataset_resource_map()
+    redirect = {}
+    for entry in collection.old_resource.entries:
+        redirect[entry["old-resource"]] = entry["resource"]
     sep = ""
     for dataset in sorted(dataset_resource):
         print(sep, end="")
@@ -36,26 +35,36 @@ def pipeline_makerules(collection):
         print()
 
         for resource in sorted(dataset_resource[dataset]):
-            fixed_path = Path("fixed") / (resource + ".csv")
-            resource_path = collection.resource_path(resource)
-            resource_path = fixed_path if os.path.isfile(fixed_path) else resource_path
-            endpoints = " ".join(collection.resource_endpoints(resource))
-            organisations = " ".join(collection.resource_organisations(resource))
-            entry_date = collection.resource_start_date(resource)
-
-            print(
-                "\n%s: %s"
-                % (
-                    transformed_path(resource, dataset),
-                    resource_path,
+            old_resource = resource
+            resource = redirect.get(resource, resource)
+            # stops resources that have been removed
+            if resource:
+                # this should be the path to the resource being processed
+                resource_path = collection.resource_path(resource)
+                endpoints = " ".join(collection.resource_endpoints(old_resource))
+                organisations = " ".join(
+                    collection.resource_organisations(old_resource)
                 )
-            )
-            print(
-                "\t$(call run-pipeline,"
-                + f" --endpoints '{endpoints}'"
-                + f" --organisations '{organisations}'"
-                + f" --entry-date '{entry_date}')"
-            )
+                entry_date = collection.resource_start_date(old_resource)
+
+                print(
+                    "\n%s: %s"
+                    % (
+                        transformed_path(old_resource, dataset),
+                        resource_path,
+                    )
+                )
+                call_pipeline = (
+                    "\t$(call run-pipeline,"
+                    + f" --endpoints '{endpoints}'"
+                    + f" --organisations '{organisations}'"
+                    + f" --entry-date '{entry_date}')"
+                )
+                # we will include the resource arguement if the old resource
+                # is  different so it's processed as the old_resource
+                if resource != old_resource:
+                    call_pipeline = call_pipeline + f"--resource {old_resource}"
+                print(call_pipeline)
 
         print("\n$(%s): $(%s)" % (dataset_var, dataset_files_var))
         print("\t$(build-dataset)")
