cc2c7cffcb62ea0c5def38a0476e79f077a58c71
eveleighoj <35256612+eveleighoj@users.noreply.github.com>
2024-10-24 09:16:46 +0100

    remove reitred resources

--- digital_land/makerules.py
+++ digital_land/makerules.py
@@ -31,7 +31,8 @@ def pipeline_makerules(collection):
         print("%s=%s" % (dataset_var, dataset_path(dataset)))
         print("%s=" % (dataset_files_var), end="")
         for resource in sorted(dataset_resource[dataset]):
-            print("\\\n    %s" % (transformed_path(resource, dataset)), end="")
+            if redirect.get(resource):
+                print("\\\n    %s" % (transformed_path(resource, dataset)), end="")
         print()
 
         for resource in sorted(dataset_resource[dataset]):
