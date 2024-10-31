4720fba91e74707c7c19800217e5bca6b138ed06
alexglasertpx <alex.glaser@tpximpact.com>
2024-10-31 16:07:04 +0000

    Edited query

--- digital_land/package/datasetparquet.py
+++ digital_land/package/datasetparquet.py
@@ -255,10 +255,11 @@ class DatasetParquetPackage(ParquetPackage):
 
         con = duckdb.connect()
 
-        drop_temp_table_query = "DROP TABLE IF EXISTS temp_table;"
-        con.query(drop_temp_table_query)
+        # drop_temp_table_query = "DROP TABLE IF EXISTS temp_table;"
+        # con.query(drop_temp_table_query)
 
         create_temp_table_query = f"""
+            DROP TABLE IF EXISTS temp_table;
             CREATE TEMP TABLE temp_table AS
             SELECT * FROM read_csv_auto('{largest_file}')
             LIMIT 1000;
