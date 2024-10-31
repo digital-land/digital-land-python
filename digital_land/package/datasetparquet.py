0c92f9dd56b51aed9abf274774cdcc31dbd58227
alexglasertpx <alex.glaser@tpximpact.com>
2024-10-31 11:41:15 +0000

    Edits to query

--- digital_land/package/datasetparquet.py
+++ digital_land/package/datasetparquet.py
@@ -241,10 +241,9 @@ class DatasetParquetPackage(ParquetPackage):
     def load_facts(self, input_paths, chunksize=chunk_size):
         logging.info(f"loading facts from {os.path.dirname(input_paths[0])}")
 
-
         fact_fields = self.specification.schema["fact"]["fields"]
         fields_str = ", ".join([f'"{field}"' if '-' in field else field for field in fact_fields])
-        input_paths_str = ", ".join([f"'{path}'" for path in input_paths])
+        input_paths_str = ', '.join([f"'{path}'" for path in input_paths])
         con = duckdb.connect()
 
         # Write a SQL query to load all parquet files from the directory, group by a field, and get the latest record
