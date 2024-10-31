3fda480f3dcac2f635a561b24ea5f09df6276abd
alexglasertpx <alex.glaser@tpximpact.com>
2024-10-31 14:42:58 +0000

    Edits to query

--- digital_land/package/datasetparquet.py
+++ digital_land/package/datasetparquet.py
@@ -267,8 +267,8 @@ class DatasetParquetPackage(ParquetPackage):
                     "start-date": "DATE",
                     "value": "VARCHAR"
                 }},
-                null_padding = true, -- pads missing columns with NULL values
-                ignore_errors = true -- ignores rows with parsing issues
+                null_padding = true -- pads missing columns with NULL values
+                -- ignore_errors = true -- ignores rows with parsing issues
             )
             QUALIFY ROW_NUMBER() OVER (PARTITION BY fact ORDER BY priority, "entry-date" DESC) = 1)
         """
