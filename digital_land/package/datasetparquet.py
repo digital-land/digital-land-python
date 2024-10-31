c8c7efbb43dcff9e144054d36fbbbbe0665430d9
alexglasertpx <alex.glaser@tpximpact.com>
2024-10-31 14:32:55 +0000

    Edits to query

--- digital_land/package/datasetparquet.py
+++ digital_land/package/datasetparquet.py
@@ -255,21 +255,21 @@ class DatasetParquetPackage(ParquetPackage):
             SELECT {fields_str} 
             FROM read_csv_auto(
                 [{input_paths_str}],
-                columns = {
-                    'end-date': 'DATE',
-                    'entity': 'BIGINT',
-                    'fact': 'VARCHAR',
-                    'field': 'VARCHAR',
-                    'entry-date': 'DATE',
-                    'priority': 'BIGINT',
-                    'reference-entity': 'VARCHAR',
-                    'start-date': 'DATE',
-                    'value': 'VARCHAR'
-                },
+                columns = {{
+                    "end-date": "DATE",
+                    "entity": "BIGINT",
+                    "fact": "VARCHAR",
+                    "field": "VARCHAR",
+                    "entry-date": "DATE",
+                    "priority": "BIGINT",
+                    "reference-entity": "VARCHAR",
+                    "start-date": "DATE",
+                    "value": "VARCHAR"
+                }},
                 null_padding = true, -- pads missing columns with NULL values
                 ignore_errors = true - - ignores rows with parsing issues
             )
-            QUALIFY ROW_NUMBER() OVER (PARTITION BY fact ORDER BY priority, 'entry-date' DESC) = 1
+            QUALIFY ROW_NUMBER() OVER (PARTITION BY fact ORDER BY priority, "entry-date" DESC) = 1
         """
 
         print("\n\nQuery")
