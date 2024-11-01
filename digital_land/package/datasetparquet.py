c80569d51e96eb9b2ef54de4faaf846629d4197d
alexglasertpx <alex.glaser@tpximpact.com>
2024-11-01 16:41:02 +0000

    Edits to query

--- digital_land/package/datasetparquet.py
+++ digital_land/package/datasetparquet.py
@@ -236,8 +236,8 @@ class DatasetParquetPackage(ParquetPackage):
             geom_columns = [row[0] for row in con.execute(geom_columns_query).fetchall()]
 
             # Export the DuckDB table to the SQLite database
-            con.execute(f"ATTACH DATABASE '{output_path}/{sqlite_file}' AS sqlite_db")
             con.execute("DROP TABLE IF EXISTS sqlite_db.my_table")
+            con.execute(f"ATTACH DATABASE '{output_path}/{sqlite_file}' AS sqlite_db")
             con.execute("CREATE TABLE sqlite_db.my_table AS SELECT * FROM temp_table")
 
         con.close()
