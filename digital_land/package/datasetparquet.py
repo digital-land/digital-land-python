892ea4aef7c783bcab9f0b2f1dab3aa26be8cb24
alexglasertpx <alex.glaser@tpximpact.com>
2024-11-04 17:46:16 +0000

    Edits to query

--- digital_land/package/datasetparquet.py
+++ digital_land/package/datasetparquet.py
@@ -260,6 +260,7 @@ class DatasetParquetPackage(ParquetPackage):
                 WITH computed_centroid AS (
                     SELECT 
                         *,
+                        EXCLUDE point,
                         CASE 
                             WHEN geometry IS NOT NULL AND point IS NULL 
                             THEN ST_AsText(ST_Centroid(ST_GeomFromText(geometry)))
