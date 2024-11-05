2554d3338dd4e8be8b5283a50cf5b3a9aec5663c
alexglasertpx <alex.glaser@tpximpact.com>
2024-11-05 13:00:22 +0000

    Edits to query

--- digital_land/package/datasetparquet.py
+++ digital_land/package/datasetparquet.py
@@ -201,7 +201,7 @@ class DatasetParquetPackage(ParquetPackage):
             PIVOT (
                 {query}
             ) ON REPLACE(field,'-','_')
-            USING LAST(value)
+            USING FIRST(value)
         """
         # sql = f"""
         #      COPY ({pivot_query}) TO '{output_path}/test2{self.suffix}' (FORMAT PARQUET);
