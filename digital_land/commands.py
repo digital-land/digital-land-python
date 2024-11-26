6168a8ed7d8a3ef95539e39ce29a54afb9180ea7
alexglasertpx <alex.glaser@tpximpact.com>
2024-11-26 11:43:49 +0000

    More print statements for debug purposes

--- digital_land/commands.py
+++ digital_land/commands.py
@@ -495,6 +495,7 @@ def dataset_dump_flattened(csv_path, flattened_dir, specification, dataset):
         print('a')
         rfc7946_geojson_path = os.path.join(flattened_dir, f"{dataset_name}.geojson")
         print('b')
+        print(get_gdal_version())
         env = (
             dict(os.environ, OGR_GEOJSON_MAX_OBJ_SIZE="0")
             if get_gdal_version() >= Version("3.5.2")
