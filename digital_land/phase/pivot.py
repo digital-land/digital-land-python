981245e31455bc1ae9e947de3b755053bda0ce20
Christopher Johns <cjohns@scottlogic.com>
2024-12-05 11:45:39 +0000

    Check for issues in the issue log.

--- digital_land/phase/pivot.py
+++ digital_land/phase/pivot.py
@@ -1,16 +1,29 @@
 from .phase import Phase
 
+EXCLUDE_ISSUES = ["invalid geometry"]
+
 
 class PivotPhase(Phase):
     """
     split entries into a series of facts
     """
 
+    def __init__(self, issue_log=None):
+        self.issue_map = {}
+        if issue_log:
+            for row in issue_log.rows:
+                self.issue_map[(row["field"], row["entry-number"])] = row
+
     def process(self, stream):
         for block in stream:
             row = block["row"]
             for field, value in sorted(row.items()):
                 if field in ["entity"]:
+                    continue
+
+                issue = self.issue_map.get((field, block["entry-number"]))
+
+                if issue and issue["issue-type"] in EXCLUDE_ISSUES:
                     continue
 
                 block["row"] = {
--- tests/unit/phase/test_pivot.py
+++ tests/unit/phase/test_pivot.py
@@ -2,16 +2,17 @@
 from copy import deepcopy
 
 from digital_land.phase.pivot import PivotPhase
+from digital_land.log import IssueLog
 
 
 def test_pivot():
     input = [
         {
-            "priority": "1",
+            "priority": 1,
             "entity": "1234",
             "resource": "res0123",
-            "line-number": "1",
-            "entry-number": "1",
+            "line-number": 1,
+            "entry-number": 1,
             "row": {
                 "entry-date": "2024-12-04",
                 "test-field": "test-value",
@@ -24,38 +25,103 @@ def test_pivot():
     assert output == [
         {
             "entity": "1234",
-            "entry-number": "1",
-            "line-number": "1",
-            "priority": "1",
+            "entry-number": 1,
+            "line-number": 1,
+            "priority": 1,
             "resource": "res0123",
             "row": {
                 "entity": "",
                 "entry-date": "2024-12-04",
-                "entry-number": "1",
+                "entry-number": 1,
                 "fact": "",
                 "field": "entry-date",
-                "line-number": "1",
-                "priority": "1",
+                "line-number": 1,
+                "priority": 1,
                 "resource": "res0123",
                 "value": "2024-12-04",
             },
         },
         {
             "entity": "1234",
-            "entry-number": "1",
-            "line-number": "1",
-            "priority": "1",
+            "entry-number": 1,
+            "line-number": 1,
+            "priority": 1,
             "resource": "res0123",
             "row": {
                 "entity": "",
                 "entry-date": "2024-12-04",
-                "entry-number": "1",
+                "entry-number": 1,
                 "fact": "",
                 "field": "test-field",
-                "line-number": "1",
-                "priority": "1",
+                "line-number": 1,
+                "priority": 1,
                 "resource": "res0123",
                 "value": "test-value",
+            },
+        },
+    ]
+
+
+def test_skips_items_in_issue_log():
+    issue_log = IssueLog()
+    issue_log.log_issue(
+        "geometry", "invalid geometry", "", "The geometry is invalid", 1, 1, "1234"
+    )
+
+    input = [
+        {
+            "priority": 2,
+            "entity": 1234,
+            "resource": "res0123",
+            "line-number": 1,
+            "entry-number": 1,
+            "row": {
+                "entry-date": "2024-12-04",
+                "geometry": "POINT (0 0)",
+                "name": "Brownfield-on-Sea",
+            },
+        },
+    ]
+
+    output = [
+        deepcopy(block) for block in PivotPhase(issue_log=issue_log).process(input)
+    ]
+
+    assert output == [
+        {
+            "entity": 1234,
+            "entry-number": 1,
+            "line-number": 1,
+            "priority": 2,
+            "resource": "res0123",
+            "row": {
+                "entity": "",
+                "entry-date": "2024-12-04",
+                "entry-number": 1,
+                "fact": "",
+                "field": "entry-date",
+                "line-number": 1,
+                "priority": 2,
+                "resource": "res0123",
+                "value": "2024-12-04",
+            },
+        },
+        {
+            "entity": 1234,
+            "entry-number": 1,
+            "line-number": 1,
+            "priority": 2,
+            "resource": "res0123",
+            "row": {
+                "entity": "",
+                "entry-date": "2024-12-04",
+                "entry-number": 1,
+                "fact": "",
+                "field": "name",
+                "line-number": 1,
+                "priority": 2,
+                "resource": "res0123",
+                "value": "Brownfield-on-Sea",
             },
         },
     ]
