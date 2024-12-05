ff82675d5ad94c3475eaa6af09c6e608b7ace9f0
eveleighoj <35256612+eveleighoj@users.noreply.github.com>
2024-12-05 11:21:42 +0000

    correct arg

--- digital_land/commands.py
+++ digital_land/commands.py
@@ -346,7 +346,7 @@ def pipeline_run(
     # create converted parquet in the var directory
     cache_dir = Path(organisation_path).parent
     transformed_parquet_dir = cache_dir / "transformed_parquet" / dataset
-    transformed_parquet_dir.mkdir(exists_ok=True, parents=True)
+    transformed_parquet_dir.mkdir(exist_ok=True, parents=True)
     convert_tranformed_csv_to_pq(
         input_path=input_path,
         output_path=transformed_parquet_dir / f"{resource}.parquet",
