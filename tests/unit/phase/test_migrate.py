a4fea33d1e92e414805bc7dcb69a57f80da0bb3d
alexglasertpx <alex.glaser@tpximpact.com>
2025-03-06 13:37:20 +0000

    Ran black on new tests

--- tests/unit/phase/test_migrate.py
+++ tests/unit/phase/test_migrate.py
@@ -1,19 +1,45 @@
 from digital_land.phase.migrate import MigratePhase
 
-field_schema = ['EndDate', 'OrganisationURI', 'entity', 'entry-date', 'organisation', 'start-date']
+field_schema = [
+    "EndDate",
+    "OrganisationURI",
+    "entity",
+    "entry-date",
+    "organisation",
+    "start-date",
+]
 
-migrations = {'organisation': 'OrganisationURI', 'end-date': 'EndDate'}
+migrations = {"organisation": "OrganisationURI", "end-date": "EndDate"}
 
 
 # Checking that fields in field_schema don't get added to the output and that None's are removed
 def test_migrate_phase():
     # Mock input stream
-    input_stream = iter([
-        {"row": {"organisation": "local-organisation:ABC", "start-date": "2022-02-02",  "entity": 12345678}},
-        {"row": {"organisation": "local-organisation:DEF", "start-date": "2022-03-02",  "entity": None}},
-        {"row": {"organisation": "local-organisation:MNO", "start-date": "2022-06-02"}},
-        {"row": {}}
-    ])
+    input_stream = iter(
+        [
+            {
+                "row": {
+                    "organisation": "local-organisation:ABC",
+                    "start-date": "2022-02-02",
+                    "entity": 12345678,
+                }
+            },
+            {
+                "row": {
+                    "organisation": "local-organisation:DEF",
+                    "start-date": "2022-03-02",
+                    "entity": None,
+                }
+            },
+            {
+                "row": {
+                    "organisation": "local-organisation:MNO",
+                    "start-date": "2022-06-02",
+                }
+            },
+            {"row": {}},
+        ]
+    )
 
     # Initialize the phase
     migrate_phase = MigratePhase(fields=field_schema, migrations=migrations)
@@ -24,17 +50,17 @@ def test_migrate_phase():
 
     # Assertions
     assert output[0]["row"] == {
-        'organisation': 'local-organisation:ABC',
-        'entity': 12345678,
-        'start-date': '2022-02-02'
+        "organisation": "local-organisation:ABC",
+        "entity": 12345678,
+        "start-date": "2022-02-02",
     }, f"First row not outputted correctly"
     assert output[1]["row"] == {
-        'organisation': 'local-organisation:DEF',
-        'start-date': '2022-03-02'
+        "organisation": "local-organisation:DEF",
+        "start-date": "2022-03-02",
     }, f"Second row not outputted correctly"
     assert output[2]["row"] == {
-        'organisation': 'local-organisation:MNO',
-        'start-date': '2022-06-02'
+        "organisation": "local-organisation:MNO",
+        "start-date": "2022-06-02",
     }, f"Third row not outputted correctly"
     assert output[3]["row"] == {}, f"Final row should not have elements"
 
--- tests/unit/phase/test_prune.py
+++ tests/unit/phase/test_prune.py
@@ -1,16 +1,46 @@
 from digital_land.phase.prune import FieldPrunePhase
 
-field_schema = ['EndDate', 'OrganisationURI', 'entity', 'entry-date', 'organisation', 'start-date']
+field_schema = [
+    "EndDate",
+    "OrganisationURI",
+    "entity",
+    "entry-date",
+    "organisation",
+    "start-date",
+]
 
 
 # Checking that None's are removed and limited to fields in field_schema
 def test_prune_phase():
     # Mock input stream
-    input_stream = iter([
-        {"row": {"organisation": "local-org:ABC", "start-date": "2022-02-02",  "entity": 12345678, "test": "XYZ"}},
-        {"row": {"organisation": None, "start-date": "2022-03-02",  "entity": 34567812, "mock-column": "AAA"}},
-        {"row": {"organisation": "local-org:MNO", "start-date": "2022-06-02",  "entity": 56781234, "mock-column": None}}
-    ])
+    input_stream = iter(
+        [
+            {
+                "row": {
+                    "organisation": "local-org:ABC",
+                    "start-date": "2022-02-02",
+                    "entity": 12345678,
+                    "test": "XYZ",
+                }
+            },
+            {
+                "row": {
+                    "organisation": None,
+                    "start-date": "2022-03-02",
+                    "entity": 34567812,
+                    "mock-column": "AAA",
+                }
+            },
+            {
+                "row": {
+                    "organisation": "local-org:MNO",
+                    "start-date": "2022-06-02",
+                    "entity": 56781234,
+                    "mock-column": None,
+                }
+            },
+        ]
+    )
 
     # Initialize the phase
     prune_phase = FieldPrunePhase(fields=field_schema)
@@ -20,16 +50,16 @@ def test_prune_phase():
 
     # Assertions
     assert output[0]["row"] == {
-        'entity': 12345678,
-        'organisation': 'local-org:ABC',
-        'start-date': '2022-02-02'
+        "entity": 12345678,
+        "organisation": "local-org:ABC",
+        "start-date": "2022-02-02",
     }, f"First row not outputted correctly"
     assert output[1]["row"] == {
-        'entity': 34567812,
-        'start-date': '2022-03-02'
+        "entity": 34567812,
+        "start-date": "2022-03-02",
     }, f"Second row not outputted correctly"
     assert output[2]["row"] == {
-        'entity': 56781234,
-        'organisation': 'local-org:MNO',
-        'start-date': '2022-06-02'
+        "entity": 56781234,
+        "organisation": "local-org:MNO",
+        "start-date": "2022-06-02",
     }, f"Third row not outputted correctly"
