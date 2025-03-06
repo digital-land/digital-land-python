cbb6e2a84e67f8b0c688afa3027346f82df961f6
alexglasertpx <alex.glaser@tpximpact.com>
2025-03-06 17:12:50 +0000

    Reverted one blank strings

--- tests/acceptance/test_add_data.py
+++ tests/acceptance/test_add_data.py
@@ -187,7 +187,7 @@ def test_cli_add_data(
         "endpoint-url": "https://www.sstaffs.gov.uk/sites/default/files/2024-11/South Staffs Conservation Area document dataset_1.csv",
         "start-date": "2022-02-02",
         "pipelines": "conservation-area",
-        "plugin": "plugin",
+        "plugin": "",
         "licence": "ogl3",
     }
     csv_path = create_input_csv(no_error_input_data)
