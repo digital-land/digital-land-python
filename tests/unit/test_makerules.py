653a2615fc0e0d53b8a10871cd7301e1c50a23ab
alexglasertpx <alex.glaser@tpximpact.com>
2025-03-18 16:24:41 +0000

    Ran black

--- tests/unit/test_makerules.py
+++ tests/unit/test_makerules.py
@@ -140,9 +140,7 @@ def test_get_processing_option_resource_change(mocker):
 
     option = get_processing_option(fake_collection, "", "", "", False, "state.json")
 
-    assert (
-        option == ProcessingOption.PROCESS_PARTIAL
-    )
+    assert option == ProcessingOption.PROCESS_PARTIAL
 
 
 def test_get_processing_option_unknown_change(mocker):
