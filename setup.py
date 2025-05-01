fc94974c8008a1231321d23a502e0689d51f394d
alexglasertpx <alex.glaser@tpximpact.com>
2025-05-01 17:21:41 +0100

    Added libraries required for testing

--- setup.py
+++ setup.py
@@ -61,7 +61,6 @@ setup(
         "boto3",
         "moto",
         "psutil",
-        "time"
     ],
     entry_points={"console_scripts": ["digital-land=digital_land.cli:cli"]},
     setup_requires=["pytest-runner"],
