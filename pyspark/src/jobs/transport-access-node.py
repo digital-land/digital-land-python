03f8561826a7c75e384ee4989b9a72b466635661
Lakshmi Kovvuri <Kumari.Lakshmi@communities.gov.uk>
2025-07-04 16:25:30 +0100

    added code for partioning and writing to S3

--- pyspark/src/jobs/transport-access-node.py
+++ pyspark/src/jobs/transport-access-node.py
@@ -3,6 +3,8 @@ from pyspark.sql.types import StructType, StructField, StringType, TimestampType
 from pyspark.sql import SparkSession
 import configparser
 import os
+from pyspark.sql.functions import first, collect_list, concat_ws
+import yaml
  
 def load_config(config_path="config.ini"):
     config = configparser.ConfigParser()
@@ -42,8 +44,33 @@ def build_schema(attributes):
 def read_data(spark, input_path):
     return spark.read.csv(input_path, header=True, inferSchema=True)
 
-def transform_data(df):
-    return df.dropna()
+def transform_data(df):    
+    # Define grouping columns (excluding 'field' and 'value')
+    group_cols = ["end-date", "entity", "entry-date", "entry-number", "priority", "reference-entity", "resource", "start-date"]
+    # Pivot the data: each 'field' becomes a column, 'value' becomes the cell value
+    pivoted_df = df.groupBy(*group_cols).pivot("field").agg(first("value"))
+    # Concatenate all 'fact' values into a single string
+    fact_concat_df = df.groupBy(*group_cols).agg(concat_ws(",", collect_list("fact")).alias("fact"))
+    # Join the pivoted data with the concatenated fact values
+    final_df = fact_concat_df.join(pivoted_df, on=group_cols, how="inner")
+    # Show the result
+    final_df.show(truncate=False)
+    return final_df
+
+def write_to_s3(df, output_path):    
+# Convert entry-date to date type and extract year, month, day
+    df = df.withColumn("entry_date", to_date("entry-date", "yyyy-MM-dd")) \
+    .withColumn("year", year("entry_date")) \
+    .withColumn("month", month("entry_date")) \
+    .withColumn("day", dayofmonth("entry_date"))
+
+# Write to S3 partitioned by year, month, day
+    df.write \
+    .partitionBy("year", "month", "day") \
+    .mode("overwrite") \
+    .option("header", "true") \
+    .parquet(output_path)
+
 
 ##writing to postgres db
 def write_to_postgres(df, config):
@@ -56,10 +83,6 @@ def write_to_postgres(df, config):
         .option("driver", config['DRIVER']) \
         .mode("overwrite") \
         .save()
-
-
-def write_to_parquet(df, output_path):    
-    df.write.mode("overwrite").parquet(output_path)
 
 def main():
     config = load_config()
@@ -77,7 +100,7 @@ def main():
     # Show schema and sample data
     processed_df.show()
     #write_to_postgres(processed_df, config)
-    write_to_parquet(processed_df, config['S3_OUTPUT_PATH'])
+    write_to_s3(processed_df, config['S3_OUTPUT_PATH'])
     spark.stop()
 
 if __name__ == "__main__":
