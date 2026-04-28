9a5b229ee70c56c6e47137b43d63d2335b6539ac
Sian Teesdale <43341988+sianteesdale@users.noreply.github.com>
2026-04-28 10:48:03 +0100

    Fixing two bugs in deleted entities expectation and update tests

--- digital_land/expectations/operations/dataset.py
+++ digital_land/expectations/operations/dataset.py
@@ -139,7 +139,7 @@ def count_deleted_entities(
     params = urllib.parse.urlencode(
         {
             "sql": f"""select * from reporting_historic_endpoints rhe join organisation o on rhe.organisation=o.organisation
-                        where pipeline == '{db_name}' and o.entity='{organisation_entity}' and resource_end_date == "" group by endpoint""",
+                        where pipeline == '{db_name}' and o.entity='{organisation_entity}' and (resource_end_date = '' or resource_end_date is null) group by endpoint""",
             "_size": "max",
         }
     )
@@ -159,10 +159,11 @@ def count_deleted_entities(
     resource_list = get_resource["resource"].to_list()
 
     # use resource list to get current entities
-    query = f"""select f.entity
+    query = f"""select e.entity
                 from fact_resource fe join fact f on fe.fact=f.fact join entity e on f.entity=e.entity
                 where resource in ({','.join(f"'{x}'" for x in resource_list)})
-                group by reference
+                  and e.organisation_entity = '{organisation_entity}'
+                group by e.entity
     """
     rows = conn.execute(query).fetchall()
     get_active_entities = [row[0] for row in rows]
